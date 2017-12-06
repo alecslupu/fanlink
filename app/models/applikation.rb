081c13f8cc7a44023fe80af13fe870f81db7b643
Mark Fraser <mf@flink.to>
2017-12-06 13:47:19 -0800

    additional app specs

--- app/models/applikation.rb
+++ app/models/applikation.rb
@@ -2,5 +2,9 @@ class Applikation < ApplicationRecord
 
   #has_paper_trail
 
+  validates :name, length: { in: 3..60, message: "must be between 3 and 60 characters" }
+
+  validates :subdomain, format: { with: /\A[a-zA-Z0-9][a-zA-Z0-9.-]+[a-zA-Z0-9]\z/ },
+            length: { in: 3..63, message: "must be between 3 and 63 characters" }
 
 end
--- spec/models/applikation_spec.rb
+++ spec/models/applikation_spec.rb
@@ -2,10 +2,52 @@ require 'rails_helper'
 
 RSpec.describe Applikation, type: :model do
 
+  describe '#name' do
+    it 'should accept a good name format' do
+      appl = create(:applikation, name: "My App")
+      expect(appl).to be_valid
+    end
+    it 'should not accept a format that is shorter than 3 characters' do
+      appl = build(:applikation, name: "aa")
+      expect(appl).not_to be_valid
+      expect(appl.errors[:name]).not_to be_blank
+    end
+    it 'should not accept a format that is longer than 60 characters' do
+      appl = build(:applikation, name: "a" * 61)
+      expect(appl).not_to be_valid
+      expect(appl.errors[:name]).not_to be_blank
+    end
+  end
+
   describe '#subdomain' do
     it 'should accept a good subdomain format' do
-      appl = FactoryBot.create(:applikation, subdomain: "goodone")
+      appl = create(:applikation, subdomain: "goodone")
       expect(appl).to be_valid
+    end
+    it 'should not accept a format that is shorter than 3 characters' do
+      appl = build(:applikation, subdomain: "aa")
+      expect(appl).not_to be_valid
+      expect(appl.errors[:subdomain]).not_to be_blank
+    end
+    it 'should not accept a format that is longer than 63 characters' do
+      appl = build(:applikation, subdomain: "a" * 64)
+      expect(appl).not_to be_valid
+      expect(appl.errors[:subdomain]).not_to be_blank
+    end
+    it 'should not start with nonalphanumeric' do
+      appl = build(:applikation, subdomain: "-abcd")
+      expect(appl).not_to be_valid
+      expect(appl.errors[:subdomain]).not_to be_blank
+    end
+    it 'should not end with nonalphanumeric' do
+      appl = build(:applikation, subdomain: "abcd-")
+      expect(appl).not_to be_valid
+      expect(appl.errors[:subdomain]).not_to be_blank
+    end
+    it 'should not contain chars other than alphanumeric or dash' do
+      appl = build(:applikation, subdomain: "ab_cd")
+      expect(appl).not_to be_valid
+      expect(appl.errors[:subdomain]).not_to be_blank
     end
   end
 
