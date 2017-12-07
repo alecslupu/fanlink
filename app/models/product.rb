481ad6e3c9d4a60938d98f9bb608ffc224962b10
mark100net <mark@pokervane.com>
2017-12-07 11:45:56 -0800

    initial rubocop config

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,10 +1,8 @@
 class Product < ApplicationRecord
-
   #has_paper_trail
 
   validates :name, length: { in: 3..60, message: "must be between 3 and 60 characters" }
 
   validates :subdomain, format: { with: /\A[a-zA-Z0-9][a-zA-Z0-9.-]+[a-zA-Z0-9]\z/ },
             length: { in: 3..63, message: "must be between 3 and 63 characters" }
-
 end
