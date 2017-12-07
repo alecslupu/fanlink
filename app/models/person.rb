05724a611816c670363e91206439cb309283fe75
mark100net <mark@pokervane.com>
2017-12-07 14:59:12 -0800

    fix factory

--- app/models/person.rb
+++ app/models/person.rb
@@ -2,4 +2,6 @@ class Person < ApplicationRecord
   authenticates_with_sorcery!
 
   acts_as_tenant(:product)
+
+  belongs_to :product, required: true
 end
--- spec/factories/people.rb
+++ spec/factories/people.rb
@@ -1,7 +1,6 @@
 FactoryBot.define do
   factory :person do
-    product FactoryGirl.create(:product)
+    product { FactoryBot.create(:product) }
     sequence(:email) { |n| "person#{n}@example.com" }
-
   end
 end
