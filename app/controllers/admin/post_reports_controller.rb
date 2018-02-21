b79e622cf73a44a378617b3ec691c4ee0b401eca
Mark Fraser <mf@flink.to>
2018-02-21 13:16:08 -0800

    proper scope for post reports

--- app/controllers/admin/post_reports_controller.rb
+++ app/controllers/admin/post_reports_controller.rb
@@ -2,6 +2,18 @@ module Admin
   class PostReportsController < Admin::ApplicationController
     include Messaging
 
+    def index
+      #search_term = params[:search].to_s.strip
+      resources = PostReport.for_product(ActsAsTenant.current_tenant).page(params[:page]).per(records_per_page)
+      page = Administrate::Page::Collection.new(dashboard, order: order)
+      render locals: {
+          resources: resources,
+          #search_term: search_term,
+          page: page,
+          show_search_bar: false
+      }
+    end
+
     def update
       post_report = PostReport.find(params["id"])
       post_report.status = params["post_report"]["status"]
--- app/models/post_report.rb
+++ app/models/post_report.rb
@@ -6,6 +6,8 @@ class PostReport < ApplicationRecord
 
   has_paper_trail
 
+  scope :for_product, -> (product) { joins([post: :person]).where("people.product_id = ?", product.id) }
+
   validates :reason, length: { maximum: 500 }
   def create_time
     created_at.to_s
