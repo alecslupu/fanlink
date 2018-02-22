b9e476dd36ffb2dd6ce06ae33db0dc5a6bfb8c31
Mark Fraser <mf@flink.to>
2018-02-21 18:06:00 -0800

    default messages in index

--- app/controllers/admin/messages_controller.rb
+++ app/controllers/admin/messages_controller.rb
@@ -12,24 +12,6 @@ module Admin
     #     per(10)
     # end
 
-    # Define a custom finder by overriding the `find_resource` method:
-    # def find_resource(param)
-    #   Message.find_by!(slug: param)
-    # end
-
-    def index
-      #search_term = params[:search].to_s.strip
-      resources = Message.for_product(ActsAsTenant.current_tenant).page(params[:page]).per(records_per_page)
-      page = Administrate::Page::Collection.new(dashboard, order: order)
-
-      render locals: {
-        resources: resources,
-        #search_term: search_term,
-        page: page,
-        show_search_bar: false
-      }
-    end
-
     def hide
       @message = Message.find(params[:message_id])
       if @message.visible?
@@ -40,7 +22,7 @@ module Admin
 
     def index
       #search_term = params[:search].to_s.strip
-      resources = Message.publics.for_product(ActsAsTenant.current_tenant).page(params[:page]).per(records_per_page)
+      resources = Message.publics.for_product(ActsAsTenant.current_tenant).reported_action_needed.page(params[:page]).per(records_per_page)
       page = Administrate::Page::Collection.new(dashboard, order: order)
 
       render locals: {
