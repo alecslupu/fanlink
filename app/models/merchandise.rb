4391f3f779576c4de58cbf4e3732f658fbe600b1
Mark Fraser <mf@flink.to>
2018-03-19 18:36:16 -0700

    merchandise priority

--- app/models/merchandise.rb
+++ app/models/merchandise.rb
@@ -1,6 +1,8 @@
 class Merchandise < ApplicationRecord
   include AttachmentSupport
 
+  after_save :adjust_priorities
+
   has_image_called :picture
 
   acts_as_tenant(:product)
@@ -8,5 +10,18 @@ class Merchandise < ApplicationRecord
   has_paper_trail
 
   scope :listable, -> { where(available: true) }
+
+private
+
+  def adjust_priorities
+    if priority > 0 && saved_change_to_attribute?(:priority)
+      same_priority = Merchandise.where.not(id: self.id).where(priority: self.priority)
+      if same_priority.count > 0
+        Merchandise.where.not(id: self.id).where("priority >= ?", self.priority).each do |m|
+          m.increment!(:priority)
+        end
+      end
+    end
+  end
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180315232120) do
+ActiveRecord::Schema.define(version: 20180320010053) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -144,6 +144,8 @@ ActiveRecord::Schema.define(version: 20180315232120) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.boolean "available", default: true, null: false
+    t.integer "priority", default: 0, null: false
+    t.index ["product_id", "priority"], name: "idx_merchandise_product_priority"
     t.index ["product_id"], name: "idx_merchandise_product"
   end
 
@@ -246,6 +248,8 @@ ActiveRecord::Schema.define(version: 20180315232120) do
     t.integer "picture_file_size"
     t.datetime "picture_updated_at"
     t.jsonb "body", default: {}, null: false
+    t.integer "priority", default: 0, null: false
+    t.index ["person_id", "priority"], name: "idx_posts_person_priority"
     t.index ["person_id"], name: "idx_posts_person"
   end
 
