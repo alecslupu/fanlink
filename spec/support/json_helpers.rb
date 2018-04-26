7902e5ff31410a559d41ba8d44e4c8f196cd5ed2
mark100net <mark@pokervane.com>
2018-04-26 11:19:00 -0700

    add num following

--- spec/support/json_helpers.rb
+++ spec/support/json_helpers.rb
@@ -141,6 +141,7 @@ module JsonHelpers
       "designation"       => (lang.present?) ? person.designation(lang) : person.designation,
       "following_id"      => (following) ? following.id : nil,
       "num_followers"     => person.followers.count,
+      "num_following"     => person.following.count,
       "badge_points"      => person.badge_points,
       "role"              => person.role,
       "level"             => (person.level.nil?) ? nil : level_json(person.level),
