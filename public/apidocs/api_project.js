39661d9cdbb75983272f17387da07b4239b6300f
Greg Diermann <greg@gregdiermann.com>
2018-04-30 16:00:49 -0500

    doc update

--- public/apidocs/api_project.js
+++ public/apidocs/api_project.js
@@ -16,7 +16,7 @@ define({
   "apidoc": "0.3.0",
   "generator": {
     "name": "apidoc",
-    "time": "2018-04-30T20:23:48.114Z",
+    "time": "2018-04-30T20:59:52.499Z",
     "url": "http://apidocjs.com",
     "version": "0.17.6"
   }
