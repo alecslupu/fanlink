5f1ecb0f1e65c5da95836ca7ad722f1a84a58be1
Mark Fraser <mf@flink.to>
2018-05-02 13:58:20 -0700

    remove person so supers can create

--- spec/factories/portal_notifications.rb
+++ spec/factories/portal_notifications.rb
@@ -1,6 +1,5 @@
 FactoryBot.define do
   factory :portal_notification do
-    person_id { create(:person).id }
     product { ActsAsTenant.current_tenant || Product.first || FactoryBot.create(:product) }
     body "My Notification"
     send_me_at { Time.now.end_of_hour }
