a0739e4954243fe4693b0391177082a15b932c71
Mark Fraser <mf@flink.to>
2018-05-03 14:42:49 -0700

    fix migration

--- db/migrate/20180503000532_change_fields_for_activity_requirement.rb
+++ db/migrate/20180503000532_change_fields_for_activity_requirement.rb
@@ -1,6 +1,6 @@
 class RenameFieldsForQuestActivity < ActiveRecord::Migration[5.1]
   def change
-    change_table :quest_activity, do |t|
+    change_table :quest_activity do |t|
       t.rename :requires, :beacon
     end
   end
