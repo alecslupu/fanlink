abce6d0bb0c35b7986f64de501a6e21daad8d8f5
Mark Fraser <mf@flink.to>
2018-05-04 14:04:41 -0700

    more logging

--- app/jobs/post_push_notification_job.rb
+++ app/jobs/post_push_notification_job.rb
@@ -2,6 +2,7 @@ class PostPushNotificationJob < Struct.new(:post_id)
   include Push
 
   def perform
+    Rails.logger.warn("performing push on post id #{post_id}")
     post = Post.find(post_id)
     ActsAsTenant.with_tenant(post.person.product) do
       if post.notify_followers
@@ -13,6 +14,7 @@ class PostPushNotificationJob < Struct.new(:post_id)
 
   def error(job, exception)
     if exception.is_a?(ActiveRecord::RecordNotFound)
+      Rails.logger.warn("rnf on #{post_id}")
       job.destroy
     end
   end
