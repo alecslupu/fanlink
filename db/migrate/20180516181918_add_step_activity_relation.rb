9d4134e4ae933d7cd5d302443b1d067456219002
Greg Diermann <greg@gregdiermann.com>
2018-05-18 08:33:53 -0500

    Removed column delete

--- db/migrate/20180516181918_add_step_activity_relation.rb
+++ db/migrate/20180516181918_add_step_activity_relation.rb
@@ -1,6 +1,6 @@
 class AddStepActivityRelation < ActiveRecord::Migration[5.1]
   def change
-    add_column :quest_activities, :step_id, :integer, null: false, default: 1
+    add_column :quest_activities, :step_id, :integer, null: false
     remove_foreign_key :quest_activities, name: "fk_activities_quests"
     add_foreign_key :quest_activities, :steps, name: "fk_activities_steps"
   end
