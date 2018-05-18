47fd3cdb0465fad474f0cc08988082df3a4277da
Greg Diermann <greg@gregdiermann.com>
2018-05-18 08:28:19 -0500

    Added drop column for preexisting step ids in activities

--- db/migrate/20180516181918_add_step_activity_relation.rb
+++ db/migrate/20180516181918_add_step_activity_relation.rb
@@ -1,7 +1,6 @@
 class AddStepActivityRelation < ActiveRecord::Migration[5.1]
   def change
-    remove_column :quest_activities, :step_id
-    add_column :quest_activities, :step_id, :integer, null: false
+    add_column :quest_activities, :step_id, :integer, null: false, default: 1
     remove_foreign_key :quest_activities, name: "fk_activities_quests"
     add_foreign_key :quest_activities, :steps, name: "fk_activities_steps"
   end
