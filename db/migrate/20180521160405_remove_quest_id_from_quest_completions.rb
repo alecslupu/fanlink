20a670bc36214d475e281eeab947b2f5cc93e530
Greg Diermann <greg@gregdiermann.com>
2018-05-21 11:06:39 -0500

    Added remove quest id migration for quest completions

--- db/migrate/20180521160405_remove_quest_id_from_quest_completions.rb
+++ db/migrate/20180521160405_remove_quest_id_from_quest_completions.rb
@@ -1,5 +1,5 @@
 class RemoveQuestIdFromQuestCompletions < ActiveRecord::Migration[5.1]
   def change
-    remove_column :quest_completion, :quest_id
+    remove_column :quest_completions, :quest_id
   end
 end
