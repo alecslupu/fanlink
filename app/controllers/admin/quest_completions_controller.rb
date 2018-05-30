d07c99fe6d14071be0fba9afa4a002b6d855cc84
Greg Diermann <greg@gregdiermann.com>
2018-05-30 17:02:50 -0500

    Added dashboard for quest completions

---
+++ app/controllers/admin/quest_completions_controller.rb
@@ -0,0 +1,21 @@
+module Admin
+  class QuestCompletionsController < Admin::ApplicationController
+    # To customize the behavior of this controller,
+    # you can overwrite any of the RESTful actions. For example:
+    #
+    # def index
+    #   super
+    #   @resources = QuestCompletion.
+    #     page(params[:page]).
+    #     per(10)
+    # end
+
+    # Define a custom finder by overriding the `find_resource` method:
+    # def find_resource(param)
+    #   QuestCompletion.find_by!(slug: param)
+    # end
+
+    # See https://administrate-prototype.herokuapp.com/customizing_controller_actions
+    # for more information
+  end
+end
