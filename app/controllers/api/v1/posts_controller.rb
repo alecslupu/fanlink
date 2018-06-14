ee9647f3ede7152613e338ca2225733e5dc00c8e
Greg Diermann <greg@gregdiermann.com>
2018-06-14 14:57:00 -0500

    Updated corsguard to only add the environment variable for portal_url if it exists. Added tag and category params to posts/lists

--- app/controllers/api/v1/posts_controller.rb
+++ app/controllers/api/v1/posts_controller.rb
@@ -242,6 +242,8 @@ class Api::V1::PostsController < ApiController
   #*
   def list
     @posts = paginate apply_filters
+    @posts = @posts.for_tag(params[:tag]) if params[:tag]
+    @posts = @posts.for_category(params[:category]) if params[:category]
     return_the @posts
   end
 
