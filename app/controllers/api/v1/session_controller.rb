9afe78e6f215f72bd9b2232bcc180f858ed183c1
Greg Diermann <greg@gregdiermann.com>
2018-06-14 22:00:04 -0500

    Moved away from cookies to headers for additional information passing

--- app/controllers/api/v1/session_controller.rb
+++ app/controllers/api/v1/session_controller.rb
@@ -2,7 +2,7 @@ class Api::V1::SessionController < ApiController
   include Rails::Pagination
   include Wisper::Publisher
   prepend_before_action :logout, only: :create
-  skip_before_action :require_login
+  skip_before_action :require_login, :set_app
   skip_before_action :set_product, :set_chewy_filter, except: %i[ create ]
   skip_after_action :unset_app, only: %i[ create destroy ]
 
