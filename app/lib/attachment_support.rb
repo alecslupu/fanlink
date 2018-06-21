2d55383d949b80d5f2f119b825f3573eede120dc
Darren Inksetter <darren@ambiguous.com>
2018-06-21 14:27:03 -0400

    misunderstood the attachment support

--- app/lib/attachment_support.rb
+++ app/lib/attachment_support.rb
@@ -5,21 +5,21 @@ module AttachmentSupport
     def has_image_called(name)
       has_attached_file name,
         :default_url => nil,
-        :styles =>  {
+        :styles => {
           optimal: "1000x",
-          thumbnail: "100x100#"
+          thumbnail: "100x100#",
         },
         :convert_options => {
-              :optimal => "-quality 75 -strip" 
+          :optimal => "-quality 75 -strip",
         }
 
       validates_attachment name,
-                           content_type: { content_type: ["image/jpeg", "image/gif", "image/png"] },
-                           size: { in: 0..5.megabytes }
+                           content_type: {content_type: ["image/jpeg", "image/gif", "image/png"]},
+                           size: {in: 0..5.megabytes}
 
       class_eval <<-EOE
         def #{name}_url
-          #{name}.file? ? #{name}.url : nil
+          #{name}.file? ? #{name}.url(:optimal) : nil
         end
         Paperclip.interpolates :product do |attachment, style|
           attachment.instance.product.internal_name
@@ -28,10 +28,10 @@ module AttachmentSupport
     end
 
     def has_audio_called(name)
-      has_attached_file name , :default_url => nil
+      has_attached_file name, :default_url => nil
       validates_attachment name,
-                          content_type: { content_type: ["audio/mpeg", "audio/mp4", "audio/aac", "audio/x-aac"] },
-                          size: { in: 0..10.megabytes }
+                           content_type: {content_type: ["audio/mpeg", "audio/mp4", "audio/aac", "audio/x-aac"]},
+                           size: {in: 0..10.megabytes}
 
       class_eval <<-EOE
         def #{name}_url
--- app/views/api/v1/posts/share.json.jbuilder
+++ app/views/api/v1/posts/share.json.jbuilder
@@ -1,6 +1,6 @@
 json.post do
   json.body @post.body(@lang)
-  json.picture_url @post.picture.url(:optimal)
+  json.picture_url @post.picture_url
   json.person do
     json.username @post.person.username
     json.picture_url @post.person.picture_url
