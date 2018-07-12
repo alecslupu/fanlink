5c52951d1f4dd049344eb380266398a4ccde8567
Greg Diermann <greg@gregdiermann.com>
2018-07-12 16:02:37 -0500

    fixes: Set adtivity types to be deleted when a quest activity is deleted.

--- app/models/step.rb
+++ app/models/step.rb
@@ -5,7 +5,7 @@ class Step < ApplicationRecord
 
   has_many :quest_activities, -> { order(created_at: :desc) }, dependent: :destroy, inverse_of: :step
 
-  has_many :quest_completions, -> { where(person_id: Person.current_user.id) }, class_name: "QuestCompletion", inverse_of: :step
+  has_many :quest_completions, -> { where(person_id: Person.current_user.id) }, dependent: :destroy, class_name: "QuestCompletion", inverse_of: :step
 
   has_one :step_completed, -> { where(person_id: Person.current_user.id) }, class_name: "StepCompleted", inverse_of: :step
 
