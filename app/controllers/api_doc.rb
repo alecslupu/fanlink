98aa46fd56b8e983703c5163a786cd0bccc9da44
Greg Diermann <greg@gregdiermann.com>
2018-07-19 16:36:41 -0500

    fixes: Only load api_doc.rb if rails env is development

--- app/controllers/api_doc.rb
+++ app/controllers/api_doc.rb
@@ -1,8 +1,10 @@
-require 'open_api/dsl'
-class ApiDoc < Object
-  include OpenApi::DSL
-  include AutoGenDoc
-  Object.const_set('Boolean', 'boolean')
-  class << self
+if Rails.env.development?
+  require 'open_api/dsl'
+  class ApiDoc < Object
+    include OpenApi::DSL
+    include AutoGenDoc
+    Object.const_set('Boolean', 'boolean')
+    class << self
+    end
   end
 end
