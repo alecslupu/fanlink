29d80c8c0f5a74d46d0fd1a9a551af65ae635b78
Greg Diermann <greg@gregdiermann.com>
2018-08-21 18:31:41 -0500

    added: Added migrations to set defaults for products and users for each of the existing rows.

--- db/migrate/20180821231807_set_product_age_requirement.rb
+++ db/migrate/20180821231807_set_product_age_requirement.rb
@@ -2,7 +2,7 @@ class SetProductAgeRequirement < ActiveRecord::Migration[5.1]
   def change
     Product.all.each do |product|
       product.age_requirement = 13
-      product.save!
+      product.save
     end
   end
 end
