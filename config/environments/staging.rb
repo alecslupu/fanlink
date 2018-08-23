d70d9444620e8c53f31b2c780a7b4c761edbf6d5
Greg Diermann <greg@gregdiermann.com>
2018-08-23 15:23:17 -0500

    fixes: asset compiling on request for staging

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -43,7 +43,7 @@ Rails.application.configure do
   config.active_support.deprecation = :notify
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  config.assets.compile = true
   config.assets.digest = true
   config.assets.js_compressor = :uglifier
 
