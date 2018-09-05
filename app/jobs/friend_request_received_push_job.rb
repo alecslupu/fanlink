a454b5e0810b41d9a95fc8eb9f05da4f80be252f
Darren Inksetter <darren@wgames.com>
2018-09-05 13:28:33 -0400

    Skip missing requests.

--- app/jobs/friend_request_received_push_job.rb
+++ app/jobs/friend_request_received_push_job.rb
@@ -2,12 +2,15 @@ class FriendRequestReceivedPushJob < Struct.new(:relationship_id)
   include Push
 
   def perform
-    relationship = Relationship.find(relationship_id)
-    ActsAsTenant.with_tenant(relationship.requested_by.product) do
-      friend_request_received_push(relationship.requested_by, relationship.requested_to)
+    relationship = Relationship.find_by_id(relationship_id)
+    unless relationship.nil?
+      ActsAsTenant.with_tenant(relationship.requested_by.product) do
+        friend_request_received_push(relationship.requested_by, relationship.requested_to)
+      end
     end
   end
 
+  #,,, not sure why this doesn't catch.
   def error(job, exception)
     if exception.is_a?(ActiveRecord::RecordNotFound)
       job.destroy
