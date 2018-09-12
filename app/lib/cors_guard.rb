4b8c94b7abb08adf65567f328f3c97dbe96b8126
Darren Inksetter <darren@wgames.com>
2018-09-12 19:56:57 -0400

    Removed caching, and the lookup of enabled product.

--- app/lib/cors_guard.rb
+++ app/lib/cors_guard.rb
@@ -1,18 +1,10 @@
 class CorsGuard
-  # TODO use redis or something for better thread safety
   ALWAYS_ALLOW = %w[ https://www.fan.link https://staging.fan.link https://portal.dev.fanlinkmusic.com https://portal.fanlinkmusic.com ]
-  CORS_KEY = "cors_allowed"
 
   ALWAYS_ALLOW << ENV["PORTAL_URL"] if ENV["PORTAL_URL"]
 
   def self.allow_from?(source)
-    true
-    #Rails.cache.fetch(CORS_KEY, expires_in: 5.minutes) do
-    #  ALWAYS_ALLOW + Product.enabled.pluck(:internal_name).map { |n| "https://#{n}.fan.link" }
-    #end.include?(source)
+    ALWAYS_ALLOW.include?(source)
   end
 
-  def self.invalidate
-    Rails.cache.delete(CORS_KEY)
-  end
 end
