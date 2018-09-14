9e7f683b21999abe346a491567a7d34063dd8d61
Darren Inksetter <darren@wgames.com>
2018-09-14 07:21:22 -0400

    I think there is a bug fix in here from Greg.

--- app/controllers/api/v3/badge_actions_controller.rb
+++ app/controllers/api/v3/badge_actions_controller.rb
@@ -41,32 +41,36 @@ class Api::V3::BadgeActionsController < Api::V3::BaseController
 
   def create
     if @rewards.any?
-      Rails.logger.error("Yes rewards")
       @rewards.each do |reward|
         if @action_type.seconds_lag > 0 && current_user.reward_progresses.where(reward_id: reward.id).where("updated_at > ?", Time.zone.now - @action_type.seconds_lag.seconds).exists?
           head :too_many_requests
           break
         else
           next if PersonReward.exists?(person_id: current_user.id, reward_id: reward.id)
-          @progress = RewardProgress.find_or_initialize_by(reward_id: reward.id, person_id: current_user.id)
-          @progress.series = @action_type.internal_name || nil
-          @progress.actions["badge_action"] ||= 0
-          @progress.actions["badge_action"] += 1
-          @progress.total ||= 0
-          @progress.total += 1
-          if @progress.present? && @progress.save
-            Rails.logger.error("Yes broadcast")
-            @series_total = RewardProgress.where(person_id: current_user.id, series: @action_type.internal_name).sum(:total) || @progress.total
-            broadcast(:reward_progress_created, current_user, @progress, @series_total)
-            return_the @progress
-          else
-            if @progress.blank?
-              render json: { errors: { base: _("Reward does not exist for that action type.") } }, status: :not_found
+          badge_action = current_user.badge_actions.create(action_type: @action_type, identifier: params[:badge_action][:identifier])
+          if badge_action.valid?
+            @progress = RewardProgress.find_or_initialize_by(reward_id: reward.id, person_id: current_user.id)
+            @progress.series = @action_type.internal_name || nil
+            @progress.actions["badge_action"] ||= 0
+            @progress.actions["badge_action"] += 1
+            @progress.total ||= 0
+            @progress.total += 1
+            if @progress.present? && @progress.save
+              @series_total = RewardProgress.where(person_id: current_user.id, series: @action_type.internal_name).sum(:total) || @progress.total
+              broadcast(:reward_progress_created, current_user, @progress, @series_total)
+              return_the @progress
             else
-              render_422(@progress.errors)
+              if @progress.blank?
+                render json: { errors: { base: _("Reward does not exist for that action type.") } }, status: :not_found
+              else
+                render_422(@progress.errors)
+              end
             end
+            break
+          else
+            render_error(badge_action.errors)
+            break
           end
-          break
         end
       end
     end
