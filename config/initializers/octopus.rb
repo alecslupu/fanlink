d4d2e8e4e73ca81618fc68bda88bb4ef71e2753b
Greg Diermann <greg@gregdiermann.com>
2018-09-17 18:11:47 -0500

    fixes: Disabled for now.

--- config/initializers/octopus.rb
+++ config/initializers/octopus.rb
@@ -1,10 +1,10 @@
-if Octopus.enabled?
-  count = case (Octopus.config[Rails.env].values[0].values[0] rescue nil)
-  when Hash
-    Octopus.config[Rails.env].map{|group, configs| configs.count}.sum rescue 0
-  else
-    Octopus.config[Rails.env].keys.count rescue 0
-  end
+# if Octopus.enabled?
+#   count = case (Octopus.config[Rails.env].values[0].values[0] rescue nil)
+#   when Hash
+#     Octopus.config[Rails.env].map{|group, configs| configs.count}.sum rescue 0
+#   else
+#     Octopus.config[Rails.env].keys.count rescue 0
+#   end
 
-  puts "=> #{count} #{'database'.pluralize(count)} enabled as read-only #{'slave'.pluralize(count)}"
-end
+#   puts "=> #{count} #{'database'.pluralize(count)} enabled as read-only #{'slave'.pluralize(count)}"
+# end
