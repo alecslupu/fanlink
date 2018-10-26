51f55761d6999262bd35df18c3d4618f97b147ec
Darren Inksetter <darren@wgames.com>
2018-10-26 09:28:37 -0400

    Inadvertant commit.

--- app/controllers/api/v3/posts_controller.rb
+++ app/controllers/api/v3/posts_controller.rb
@@ -176,7 +176,7 @@ class Api::V3::PostsController < Api::V2::PostsController
       @posts = paginate(Post.visible.following_and_own(current_user).unblocked(current_user.blocked_people).order(created_at: :desc))
     end
     @post_reactions = current_user.post_reactions.where(post_id: @posts).index_by(&:post_id)
-    return_the @posts, handler: "jb"
+    return_the @posts
   end
   # **
   # @api {get} /posts/list Get a list of posts (ADMIN ONLY).
