03a9981c1bff5ad518d8355646233a78424aae26
Greg Diermann <greg@gregdiermann.com>
2018-10-26 15:50:59 -0500

    added: Order field to rooms

--- app/views/api/v3/rooms/_room.json.jbuilder
+++ app/views/api/v3/rooms/_room.json.jbuilder
@@ -4,6 +4,7 @@ json.description room.description(@lang)
 json.owned room.created_by_id == current_user.id
 json.picture_url room.picture_url
 json.public room.public
+json.order room.order if room.public?
 if room.private?
   json.members room.members, partial: "api/v3/people/person", as: :person
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20181024012406) do
+ActiveRecord::Schema.define(version: 20181026204010) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -711,6 +711,7 @@ ActiveRecord::Schema.define(version: 20181024012406) do
     t.datetime "picture_updated_at"
     t.jsonb "name", default: {}, null: false
     t.jsonb "description", default: {}, null: false
+    t.integer "order", default: 0, null: false
     t.index ["created_by_id"], name: "index_rooms_on_created_by_id"
     t.index ["product_id", "status"], name: "unq_rooms_product_status"
   end
