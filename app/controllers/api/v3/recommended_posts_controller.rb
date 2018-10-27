c0f2318c6b78f01aa26bb61101fd50386ba9db7d
Greg Diermann <greg@gregdiermann.com>
2018-10-26 19:56:49 -0500

    fixes: Javascript on my mind...

--- app/controllers/api/v3/recommended_posts_controller.rb
+++ app/controllers/api/v3/recommended_posts_controller.rb
@@ -25,9 +25,9 @@ class Api::V3::RecommendedPostsController < Api::V2::RecommendedPostsController
   def index
     if %w[ lvconnect nashvilleconnect ].include?(ActsAsTenant.current_tenant.internal_name) {
       @posts paginate Post.for_product(ActsAsTenant.current_tenant).visible.order(created_at: :desc)
-    } else {
+    else
       @posts = paginate Post.for_product(ActsAsTenant.current_tenant).visible.where(recommended: true).order(created_at: :desc)
-    }
+    end
     @post_reactions = current_user.post_reactions.where(post_id: @posts).index_by(&:post_id)
     return_the @posts
   end
