f35e30fabfddbdfa380544e4dfc8a0d22fe9a21d
Greg Diermann <greg@gregdiermann.com>
2018-10-26 19:49:20 -0500

    added: checks for lv and nashville connect products to change recommended posts for this weekened

--- app/controllers/api/v3/recommended_posts_controller.rb
+++ app/controllers/api/v3/recommended_posts_controller.rb
@@ -23,7 +23,11 @@ class Api::V3::RecommendedPostsController < Api::V2::RecommendedPostsController
   # *
 
   def index
-    @posts = paginate Post.for_product(ActsAsTenant.current_tenant).visible.where(recommended: true).order(created_at: :desc)
+    if %w[ lvconnect nashvilleconnect ].include?(ActsAsTenant.current_tenant.internal_name.) {
+      @posts paginate Post.for_product(ActsAsTenant.current_tenant).visible.order(created_at: :desc)
+    } else {
+      @posts = paginate Post.for_product(ActsAsTenant.current_tenant).visible.where(recommended: true).order(created_at: :desc)
+    }
     @post_reactions = current_user.post_reactions.where(post_id: @posts).index_by(&:post_id)
     return_the @posts
   end
