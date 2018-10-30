4fa226b3a9362d3bb6b61c1826d946b4cda01ef3
Darren Inksetter <darren@wgames.com>
2018-10-29 22:10:22 -0400

    why is this so difficult?`

--- app/controllers/aws_controller.rb
+++ app/controllers/aws_controller.rb
@@ -9,6 +9,7 @@ class AwsController < ApplicationController
 	# Authentication is done through basic auth with an `ApiKey`
 	#
 	def video_transcoded
+    request.format = :json
     Rails.logger.error "\n\nBODY IS\n\n#{request.body.read.inspect}\n\n"
 		type  = request.headers['x-amz-sns-message-type'].to_s.downcase
 		topic = request.headers['x-amz-sns-topic-arn']
