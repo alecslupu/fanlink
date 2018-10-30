9f5d002560a2397072027600e11ca29920831942
Darren Inksetter <darren@wgames.com>
2018-10-29 22:03:44 -0400

    read the body

--- app/controllers/aws_controller.rb
+++ app/controllers/aws_controller.rb
@@ -12,7 +12,7 @@ class AwsController < ApplicationController
     Rails.logger.error "\n\nBODY IS\n\n#{request.body.read.inspect}\n\n"
 		type  = request.headers['x-amz-sns-message-type'].to_s.downcase
 		topic = request.headers['x-amz-sns-topic-arn']
-		body  = JSON.parse(request.read)
+    body  = JSON.parse(request.body.read)
 		if(type == 'subscriptionconfirmation')
 			Flaws.sns_confirm(topic, body['Token'])
 		elsif(type == 'notification')
