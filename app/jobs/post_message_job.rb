48517f9a554e8b5f5a0fd5ff27a86f6914b4823f
Greg Diermann <greg@gregdiermann.com>
2018-11-29 22:07:07 -0600

    fixes: Hack for firebase to parse all messages in v3 mention format. Deleted v4 quest views. Commented out v4 controller code.

--- app/jobs/post_message_job.rb
+++ app/jobs/post_message_job.rb
@@ -5,7 +5,45 @@ class PostMessageJob < Struct.new(:message_id)
     message = Message.find(message_id)
     ActsAsTenant.with_tenant(message.room.product) do
       if message.room.public?
-        client.set("#{room_path(message.room)}/last_message", message.as_json)
+        msg = {
+          id: message.id,
+          body: message.parse_content(3),
+          # picture_id: message.picture_id,
+          create_time: message.create_time,
+          picture_url: message.picture_url,
+          pinned: message.pinned,
+          person: {
+            id: message.person.id,
+            username: message.person.username,
+            name: message.person.name,
+            designation: message.person.designation,
+            product_account: message.person.product_account,
+            chat_banned: message.person.chat_banned,
+            badge_points: message.person.badge_points,
+            level: message.person.level,
+            picture_url: message.person.picture_url
+          }
+        }
+        if message.mention_meta.length > 0
+          msg[:message_mentions] = message.mention_meta.map do |mention|
+            {
+              id: mention.id,
+              length: mention.length,
+              location: mention.location,
+              person_id: mention.person_id
+            }
+          end
+        else
+          msg[:message_mentions] = nil
+        end
+        # msg = ApiController.render(
+        #   template: "api/v4/messages/show",
+        #   assigns: {message: message},
+        #   handlers: "jb"
+        # )
+        Rails.logger.debug(msg)
+        res = client.set("#{room_path(message.room)}/last_message", msg)
+        Rails.logger.debug(res)
       else
         client.set("#{room_path(message.room)}/last_message_id", message.id)
       end
