11ca2c123149dbf1ed7d3a8081fc3113412d075d
AlexM <lugerand2004@yahoo.com>
2019-01-16 13:26:20 +0100

    FLAPI-197 fix duplicate votes

--- app/controllers/api/v4/poll_options_controller.rb
+++ app/controllers/api/v4/poll_options_controller.rb
@@ -1,10 +1,14 @@
 class Api::V4::PollOptionsController < Api::V3::PollOptionsController 
   def cast_vote
     @vote = PersonPollOption.create(person_id: current_user.id, poll_option_id: params[:poll_option_id])
-    if @vote.valid?
+    if current_user.poll_options.where(poll_id: @poll.id)
+      render json: {error: "already voted on this poll"}
+    elsif @vote.valid?
       return_the @vote, handler: 'jb', using: :cast_vote
     else
       render_422 @vote.errors
     end
   end
 end
+
+
--- app/models/person_poll_option.rb
+++ app/models/person_poll_option.rb
@@ -1,5 +1,6 @@
 class PersonPollOption < ApplicationRecord
   belongs_to :poll_option
   belongs_to :person
+  validates_uniqueness_of :poll_option_id, :scope => :person_id
 end
 
