c953bb5a103245355cb931ce70efc58d9ffd0246
AlexM <lugerand2004@yahoo.com>
2019-01-22 20:29:35 +0100

    FLAPI-197 fixed vote unicity

--- app/controllers/api/v4/poll_options_controller.rb
+++ app/controllers/api/v4/poll_options_controller.rb
@@ -1,9 +1,10 @@
 class Api::V4::PollOptionsController < Api::V3::PollOptionsController 
   def cast_vote
-    @vote = PersonPollOption.create(person_id: current_user.id, poll_option_id: params[:poll_option_id])
-    if current_user.poll_options.where(poll_id: @poll.id)
+    @vote = PersonPollOption.new(person_id: current_user.id, poll_option_id: params[:poll_option_id])
+    if !current_user.poll_options.where(poll_id: @poll.id).blank?
       render json: {error: "already voted on this poll"}
     elsif @vote.valid?
+      @vote.save
       return_the @vote, handler: 'jb', using: :cast_vote
     else
       render_422 @vote.errors
