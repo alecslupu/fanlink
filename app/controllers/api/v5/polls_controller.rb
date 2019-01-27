766cd1c2e024bed29d4228133788689104da5be3
Greg Diermann <greg@gregdiermann.com>
2019-01-26 18:00:50 -0600

    changes: V5 web view changes.

--- app/controllers/api/v5/polls_controller.rb
+++ app/controllers/api/v5/polls_controller.rb
@@ -1,2 +1,41 @@
 class Api::V5::PollsController < Api::V4::PollsController
+
+
+  def create
+    parms = poll_params
+    @poll = Poll.create(parms)
+    @poll.poll_type_id = params[params[:poll][:poll_type]+"_id"]
+    if @poll.valid?
+      @poll.save
+      return_the @poll, handler: 'jb', using: :show
+    else
+      render_422 @poll.errors
+    end
+  end
+
+  def destroy
+    if @post.person == current_user
+      @poll.destroy
+      head :ok
+    else
+      render_not_found
+    end
+  end
+
+  def update
+    if params.has_key?(:poll)
+      if @poll.update_attributes(poll_params)
+        return_the @poll
+      else
+        render_422 @poll.errors
+      end
+    else
+      return_the @poll, handler: 'jb', using: :show
+    end
+  end
+
+  def index
+    @polls = paginate(Poll.all.order(created_at: :asc))
+    return_the @polls, handler: 'jb'
+  end
 end
