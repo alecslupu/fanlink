ec2a8a641955874df506ec0e605d955f05e34c30
Greg Diermann <greg@gregdiermann.com>
2019-01-26 18:31:54 -0600

    fixes: wrong type syntax. Damn you old documentation

--- app/graphql/types/message_type.rb
+++ app/graphql/types/message_type.rb
@@ -2,18 +2,32 @@ module Types
   class MessageType < Types::BaseObject
     graphql_name "Post"
     description "User Created Posts"
-    field :id, types.ID, null: false
-    field :body, types.String
-    field :global, !types.Boolean
-    field :status, types.String
-    field :recommended, !types.Boolean
-    field :notify_followers, !types.Boolean
+    field :id, ID, null: false
+    field :body, String, null: true
+    field :global, Boolean, null: true
+    field :status, String, null: true
+    field :recommended, Boolean, null: true
+    field :notify_followers, Boolean, null: true
+    field :priority, Integer, null: false
+    field :create_time, String, null: false
+    field :picture_url, String, null: true
+    field :audio_url, String, null: true
+    field :audio_size, Integer, null: true
+    field :audio_content_type, String, null: true
+    field :video_url, String, null: true
+    field :comment_count, Integer, null: false
+    field :total, Integer, null: false
 
-    field :picture_url, !types.String
-    field :audio_url, !types.String
-    field :audio_size, !types.Int
-    field :audio_content_type, !types.String
-    field :video_url, !types.String
+    def comment_count
+      object.post_comments_count
+    end
 
+    def total
+      object.size
+    end
+
+    def create_time
+      object.created_at.to_s
+    end
   end
 end
