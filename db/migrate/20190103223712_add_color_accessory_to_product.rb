18d5bbd32b47b3bcd901e5f8559ee6fe0512f874
Greg Diermann <greg@gregdiermann.com>
2019-02-06 19:43:43 -0600

    fixed: Removed color accessory migration

--- db/migrate/20190103223712_add_color_accessory_to_product.rb
+++ db/migrate/20190103223712_add_color_accessory_to_product.rb
@@ -1,5 +1,5 @@
 class AddColorAccessoryToProduct < ActiveRecord::Migration[5.1]
   def change
-    add_column :products, :color_accessory, :string, default: '000000'
+    # add_column :products, :color_accessory, :string, default: '000000'
   end
 end
