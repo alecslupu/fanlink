bf7af13da81a4ebd729e5a71bc216577066a45f5
Greg Diermann <greg@gregdiermann.com>
2019-02-11 00:54:16 -0600

    fixed: Added production seed files

---
+++ db/seeds/quest_completed.rb
@@ -0,0 +1,8 @@
+QuestCompleted.create!([
+  {quest_id: 12, person_id: 380},
+  {quest_id: 19, person_id: 436},
+  {quest_id: 19, person_id: 287},
+  {quest_id: 20, person_id: 460},
+  {quest_id: 18, person_id: 288},
+  {quest_id: 18, person_id: 455}
+])
