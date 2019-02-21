8b2ff437f8f50df4022f446a7b13446158cafde3
AlexM <lugerand2004@yahoo.com>
2019-02-21 10:35:12 +0100

    FLAPI-764 fixed mock

--- app/controllers/api/v4/certificates_controller.rb
+++ app/controllers/api/v4/certificates_controller.rb
@@ -2,8 +2,8 @@ class Api::V4::CertificatesController < ApiController
   def index
     @certificates = paginate Certificate.all
     #return_the @certificates, handler: 'jb'
-    render json: {"certificates": [{"id":"1","order":"1","long_name":"Certificate Dummy Long Name","short_name":"Short name Dummy",
-      "description":"Dummy Test","color_hex":"#FF00FF","chat_room_id":"1493", "sku_android":"", "sku_ios":"", "is_free":"false","is_completed":"true","is_purchased":"true","certificate_image_url":""}]}
+    render json: {"certificates": [{"id":1,"order":1,"long_name":"Certificate Dummy Long Name","short_name":"Short name Dummy",
+      "description":"Dummy Test","color_hex":"#FF00FF","chat_room_id":1493, "sku_android":"", "sku_ios":"", "is_free":false,"is_completed":true,"is_purchased":true,"certificate_image_url":""}]}
   end
 
   def show
