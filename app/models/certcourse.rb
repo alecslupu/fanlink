d5951a21486cd5898d41ebe83b89a169b8c5f107
AlexM <lugerand2004@yahoo.com>
2019-02-27 18:21:36 +0100

    FLAPI-764 improving the person-certcourse

--- app/models/certcourse.rb
+++ app/models/certcourse.rb
@@ -2,6 +2,9 @@ class Certcourse < ApplicationRecord
   has_many :certificate_certcourses
   has_many :certificates, through: :certificate_certcourses, dependent: :destroy
 
+  has_many :person_certcourses
+  has_many :people, through: :person_certcourses, dependent: :destroy
+
   has_many :certcourse_pages
 
   enum status: %i[entry live]
