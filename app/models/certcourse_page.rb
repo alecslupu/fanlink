0635fb5bf5f38555bc10b294fd5683d054863dd1
AlexM <lugerand2004@yahoo.com>
2019-02-27 16:06:14 +0100

    FLAPI-764 fixing person certificate endpoint and completing some views

--- app/models/certcourse_page.rb
+++ app/models/certcourse_page.rb
@@ -4,4 +4,18 @@ class CertcoursePage < ApplicationRecord
   has_one :quiz_page
   has_one :video_page
   has_one :image_page
+
+  def content_type
+  	return 'quiz' if quiz_page.present? 
+  	return 'video' if video_page.present?
+  	return 'image' if image_page.present?
+  end
+
+  def media_url
+  	unless quiz_page.present?
+  		"media url"
+  	else
+  		null
+  	end
+  end
 end
