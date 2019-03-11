58ea3ae7b8aca4fdc7eca771db8961505a2b2040
AlexM <lugerand2004@yahoo.com>
2019-03-11 09:43:42 +0100

    FLAPI-764 moved product from private to public

--- app/models/video_page.rb
+++ app/models/video_page.rb
@@ -14,8 +14,16 @@ class VideoPage < ApplicationRecord
   def product
   	Product.find_by(internal_name: "caned")
   end
- 
+
   private
+
+  def just_me
+    x = CertcoursePage.find(certcourse_page.id)
+    child = x.child
+    if child && child != self
+      errors.add(:base, :just_me, message: _("A page can only have one of video, image, or quiz"))
+    end
+  end
 
   def set_certcourse_page_content_type
   	page = CertcoursePage.find(certcourse_page_id)
