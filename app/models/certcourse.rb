9d8c10de5f9f3e0c676b07af2426bdf8510118ab
Darren Inksetter <darren@wgames.com>
2019-03-14 11:29:16 -0400

    Validate color hex

--- app/models/certcourse.rb
+++ app/models/certcourse.rb
@@ -13,6 +13,7 @@ class Certcourse < ApplicationRecord
   enum status: %i[entry live]
 
   validates :long_name, :short_name, :description, :color_hex, :status, :duration, :copyright_text, presence: true
+  validates_format_of :color_hex, with: /\A#(?:[A-F0-9]{3}){1,2}\z/i
 
   scope :live_status, -> { where(status: "live") }
 end
