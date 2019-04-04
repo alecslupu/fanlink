0d62fbe794b0000db06c8fc0a79b6ec1ad901da5
Alexandru-Emil Lupu <contact@alecslupu.ro>
2019-04-04 20:09:57 +0300

    Changed the migration

--- db/migrate/20190401193436_create_trivia_games.rb
+++ db/migrate/20190401193436_create_trivia_games.rb
@@ -4,7 +4,6 @@ class CreateTriviaGames < ActiveRecord::Migration[5.1]
     create_table :trivia_games do |t|
       t.datetime :start_date
       t.datetime :end_date
-      t.string :name
       t.text :description
       t.integer :package_count
       t.string :long_name, null: false
