f520c792dfa29f16836e8e357ec15fdd95057537
Alexandru-Emil Lupu <contact@alecslupu.ro>
2019-04-07 20:52:08 +0300

    Adding recommended post specs

--- spec/factories/posts.rb
+++ spec/factories/posts.rb
@@ -3,5 +3,13 @@ FactoryBot.define do
     person { create(:person) }
     body { "MyText" }
     global { false }
+
+    factory :recommended_post do
+      recommended { true }
+    end
+
+    factory :published_post do
+      status { :published }
+    end
   end
 end
