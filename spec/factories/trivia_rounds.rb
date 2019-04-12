692818d7cc225ecd567ecef2538c65d9a427dfe0
Alexandru-Emil Lupu <contact@alecslupu.ro>
2019-04-13 02:17:32 +0300

    annotation on trivia round

--- spec/factories/trivia_rounds.rb
+++ spec/factories/trivia_rounds.rb
@@ -1,3 +1,21 @@
+# == Schema Information
+#
+# Table name: trivia_rounds
+#
+#  id               :bigint(8)        not null, primary key
+#  start_date       :datetime
+#  end_date         :datetime
+#  question_count   :integer
+#  trivia_game_id   :bigint(8)
+#  leaderboard_size :integer          default(100)
+#  package_order    :integer          default(1), not null
+#  status           :integer          default("draft"), not null
+#  uuid             :uuid
+#  created_at       :datetime         not null
+#  updated_at       :datetime         not null
+#  complexity       :integer          default(1)
+#
+
 FactoryBot.define do
   factory :trivia_round, class: "Trivia::Round" do
     start_date { "2019-04-01 22:36:15" }
