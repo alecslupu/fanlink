4aead3622aa62778d345ef9a25f1ce4434eeb2d1
Alexandru-Emil Lupu <contact@alecslupu.ro>
2019-05-01 23:40:01 +0300

    Fixing broken tests

--- spec/middleware/sns_content_type_spec.rb
+++ spec/middleware/sns_content_type_spec.rb
@@ -6,24 +6,12 @@ describe SnsContentType do
 
   # TODO: auto-generated
   describe '#new' do
-    it 'works' do
-      pending
-      # app = double('app')
-      # result = SnsContentType.new(app)
-      # expect(result).not_to be_nil
-    end
+    pending
   end
 
   # TODO: auto-generated
   describe '#call' do
-    it 'works' do
-      pending
-      # app = double('app')
-      # sns_content_type = SnsContentType.new(app)
-      # env = double('env')
-      # result = sns_content_type.call(env)
-      # expect(result).not_to be_nil
-    end
+    pending
   end
 
 end
--- spec/services/token_provider_spec.rb
+++ spec/services/token_provider_spec.rb
@@ -6,22 +6,12 @@ describe TokenProvider do
 
   # TODO: auto-generated
   describe '#issue_token' do
-    it 'works' do
-      pending
-      # payload = double('payload')
-      # result = TokenProvider.issue_token(payload)
-      # expect(result).not_to be_nil
-    end
+    pending
   end
 
   # TODO: auto-generated
   describe '#valid?' do
-    it 'works' do
-      pending
-      # token = double('token')
-      # result = TokenProvider.valid?(token)
-      # expect(result).not_to be_nil
-    end
+    pending
   end
 
 end
