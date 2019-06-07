620059adfa7a68a3b5101e9a954e9d74884654a7
Alexandru-Emil Lupu <contact@alecslupu.ro>
2019-06-07 11:41:19 +0300

    policy update

--- app/policies/trivia/game_policy.rb
+++ app/policies/trivia/game_policy.rb
@@ -6,7 +6,8 @@ module Trivia
 
     class Scope < ApplicationPolicy::Scope
       def resolve
-        super.for_product(ActsAsTenant.current_tenant).includes(rounds: [questions: [ :available_question => :available_answers ]])
+        # super.for_product(ActsAsTenant.current_tenant).includes(rounds: [questions: [ :available_question => :available_answers ]])
+	super.for_product(ActsAsTenant.current_tenant)
       end
     end
   end
