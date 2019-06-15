5a1c24d9a65392ceff75459ddd54526c15c10dbd
Alexandru-Emil Lupu <contact@alecslupu.ro>
2019-06-15 15:25:25 +0300

    CORS + topic reimport

--- app/lib/cors_guard.rb
+++ app/lib/cors_guard.rb
@@ -1,5 +1,18 @@
 class CorsGuard
-  ALWAYS_ALLOW = %w[ https://www.fan.link https://staging.fan.link https://portal.dev.fanlinkmusic.com https://portal.staging.fanlinkmusic.com https://portal.fanlinkmusic.com ]
+  ALWAYS_ALLOW = %w(
+    https://www.fan.link
+    https://staging.fan.link
+    https://portal.dev.fanlinkmusic.com
+    https://portal.staging.fanlinkmusic.com
+    https://portal.fanlinkmusic.com
+  )
+
+  %w(
+    http://alecslupu.go.ro
+    http://alecslupu.go.ro:8082
+  ).each do |url|
+    ALWAYS_ALLOW << url
+  end
 
   if ENV["PORTAL_URL"]
     ENV["PORTAL_URL"].split(",").each do |url|
