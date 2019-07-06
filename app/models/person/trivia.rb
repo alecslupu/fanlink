6e61a15e8ea446cca1f107b39ecaab8c278e72c9
Alexandru-Emil Lupu <contact@alecslupu.ro>
2019-07-06 21:09:27 +0300

    FLAPI-988

--- app/models/person/trivia.rb
+++ app/models/person/trivia.rb
@@ -6,7 +6,6 @@ module Person::Trivia
     has_many :trivia_package_leaderboards, class_name: "Trivia::RoundLeaderboard"
     has_many :trivia_question_leaderboards, class_name: "Trivia::QuestionLeaderboard"
     has_many :trivia_answers, class_name: "Trivia::Answer"
-    has_many :trivia_participantions, class_name: "Trivia::Participant"
     has_many :trivia_subscribers, class_name: "Trivia::Subscriber"
   end
 end
