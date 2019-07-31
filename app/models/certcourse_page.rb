d743dcbbf3e3ffcd79bacc94f9a425aea8fef3f7
Alexandru-Emil Lupu <contact@alecslupu.ro>
2019-07-31 22:18:07 +0300

    Fixing FLAPI-1022

--- app/models/certcourse_page.rb
+++ app/models/certcourse_page.rb
@@ -23,15 +23,18 @@ class CertcoursePage < ApplicationRecord
   has_one :quiz_page
   has_one :video_page
   has_one :image_page
+  has_one :download_file_page
   has_many :course_page_progresses
 
   accepts_nested_attributes_for :quiz_page, allow_destroy: true
   accepts_nested_attributes_for :video_page, allow_destroy: true
   accepts_nested_attributes_for :image_page, allow_destroy: true
+  accepts_nested_attributes_for :download_file_page, allow_destroy: true
 
   scope :quizes, -> { joins(:quiz_page) }
   scope :videos, -> { joins(:video_page) }
   scope :images, -> { joins(:image_page) }
+  scope :download_files, -> { joins(:download_file_page) }
   scope :ordered, -> { order(:certcourse_page_order) }
   scope :for_product, -> (product) { where(product_id: product.id) }
 
@@ -40,31 +43,46 @@ class CertcoursePage < ApplicationRecord
   validate :single_child_validator
 
   def content_type
-    return "quiz" if quiz?
-    return "video" if video_page.present?
-    return "image" if image_page.present?
+    child.content_type
+    # return "image" if image?
+    # return "download_file" if download?
   end
 
   validates_format_of :background_color_hex, with: /\A#?(?:[A-F0-9]{3}){1,2}\z/i
 
   def media_url
-    return if quiz?
+    return image_page.image_url if image?
+    return video_page.video_url if video?
+    return download_file_page.document_url if download?
 
-    "media url"
+    nil
+  end
+
+  def download?
+    download_file_page.present?
+  end
+
+  def video?
+    video_page.present?
   end
 
   def child
-    quiz_page || image_page || video_page
+    quiz_page || image_page || video_page || download_file_page
   end
 
   def quiz?
     quiz_page.present?
   end
 
+  def image?
+    image_page.present?
+  end
+
   protected
   def single_child_validator
-    errors.add(:base, _("You cannot add a question and a video or imaage on the same certcourse")) if quiz_page.present? && (video_page.present? || image_page.present?)
-    errors.add(:base, _("You cannot add an image and a video or question on the same certcourse")) if image_page.present? && (quiz_page.present? || video_page.present?)
-    errors.add(:base, _("You cannot add a video and a question or imaage on the same certcourse")) if video_page.present? && (quiz_page.present? || image_page.present?)
+    errors.add(:base, _("You cannot add a question and a video or imaage on the same certcourse")) if quiz? && (download? || video? || image?)
+    errors.add(:base, _("You cannot add an image and a video or question on the same certcourse")) if image? && (download? || quiz? || video?)
+    errors.add(:base, _("You cannot add a video and a question or imaage on the same certcourse")) if video? && (download? || quiz? || image?)
+    errors.add(:base, _("You cannot add a download and a question or imaage on the same certcourse")) if download? && (video? || quiz? || image?)
   end
 end
