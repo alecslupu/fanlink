5863f65c80d45d28d2323ff152e422b0a41ddfa6
Alexandru-Emil Lupu <contact@alecslupu.ro>
2019-08-07 07:28:16 +0300

    Fixing FLAPI-1049

--- app/models/portal_access.rb
+++ app/models/portal_access.rb
@@ -15,6 +15,8 @@
 #  beacon      :integer          default(0), not null
 #  reporting   :integer          default(0), not null
 #  interest    :integer          default(0), not null
+#  courseware  :integer          default(0), not null
+#  trivia      :integer          default(0), not null
 #
 
 class PortalAccess < ApplicationRecord
@@ -23,60 +25,13 @@ class PortalAccess < ApplicationRecord
   scope :for_product, -> (product) { joins(:person).where(people: { product_id: product.id }) }
   belongs_to :person
 
-  has_flags 1 => :post_read,
-            2 => :post_update,
-            3 => :post_delete,
-            :column => "post"
-
-  has_flags 1 => :chat_read,
-            2 => :chat_update,
-            3 => :chat_delete,
-            :column => "chat"
-
-  has_flags 1 => :event_read,
-            2 => :event_update,
-            3 => :event_delete,
-            :column => "event"
-
-  has_flags 1 => :merchandise_read,
-            2 => :merchandise_update,
-            3 => :merchandise_delete,
-            :column => "merchandise"
-
-  has_flags 1 => :user_read,
-            2 => :user_update,
-            3 => :user_delete,
-            :column => "user"
-
-  has_flags 1 => :badge_read,
-            2 => :badge_update,
-            3 => :badge_delete,
-            :column => "badge"
-
-  has_flags 1 => :reward_read,
-            2 => :reward_update,
-            3 => :reward_delete,
-            :column => "reward"
-
-  has_flags 1 => :quest_read,
-            2 => :quest_update,
-            3 => :quest_delete,
-            :column => "quest"
-
-  has_flags 1 => :beacon_read,
-            2 => :beacon_update,
-            3 => :beacon_delete,
-            :column => "beacon"
-
-  has_flags 1 => :reporting_read,
-            2 => :reporting_update,
-            3 => :reporting_delete,
-            :column => "reporting"
-
-  has_flags 1 => :interest_read,
-            2 => :interest_update,
-            3 => :interest_delete,
-            :column => "interest"
+  %w(post chat event merchandise user badge reward quest beacon reporting interest courseware trivia).each do |field|
+    has_flags 1 => "#{field}_read".to_sym,
+              2 => "#{field}_update".to_sym,
+              3 => "#{field}_delete".to_sym,
+              4 => "#{field}_export".to_sym,
+              :column => field
+  end
 
   def summarize
     perms = {}
