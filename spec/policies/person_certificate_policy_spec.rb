6f038b99752db3d0cc84050fb0ec1e96e584edd4
Cristian Somesan <crs@flink.to>
2019-08-27 19:07:50 +0300

    Add policy tests for person certificate and person poll option

--- spec/policies/person_certificate_policy_spec.rb
+++ spec/policies/person_certificate_policy_spec.rb
@@ -1,27 +1,74 @@
-require 'rails_helper'
+require "rails_helper"
 
 RSpec.describe PersonCertificatePolicy, type: :policy do
-  let(:user) { User.new }
+  subject { described_class.new(person, person_certificate) }
 
-  subject { described_class }
+  let(:person) { nil }
 
-  permissions ".scope" do
-    pending "add some examples to (or delete) #{__FILE__}"
+  context "CRUD actions" do
+    let(:person_certificate) { PersonCertificate.new }
+
+    it { is_expected.to permit_new_and_create_actions }
+    it { is_expected.to permit_edit_and_update_actions }
+    it { is_expected.to forbid_action(:destroy) }
+    it { is_expected.to permit_action(:index) }
+    it { is_expected.to permit_action(:show) }
   end
 
-  permissions :show? do
-    pending "add some examples to (or delete) #{__FILE__}"
+  context "Rails admin actions" do
+    let(:person_certificate) { PersonCertificate.new }
+
+    it { is_expected.to permit_actions(%i[export history dashboard select_product_dashboard]) }
+    it { is_expected.to forbid_actions(%i[show_in_app generate_game_action]) }
   end
 
-  permissions :create? do
-    pending "add some examples to (or delete) #{__FILE__}"
+  describe "#select_product" do
+    let(:person_certificate) { PersonCertificate.new }
+
+    context "superadmin who has the admin product assigned" do
+      let(:product) { create(:product, internal_name: "admin") }
+      let(:person) { Person.new(product: product, role: :super_admin) }
+
+      it { is_expected.to permit_action(:select_product) }
+    end
+
+    context "admin who has the admin product assigned" do
+      let(:product) { create(:product, internal_name: "admin") }
+      let(:person) { Person.new(product: product, role: :admin) }
+
+      it { is_expected.to forbid_action(:select_product) }
+    end
+
+    context "superadmin who doesn't have admin product assigned" do
+      let(:product) { create(:product, internal_name: "not_admin") }
+      let(:person) { Person.new(product: product, role: :super_admin) }
+
+      it { is_expected.to forbid_action(:select_product) }
+    end
   end
 
-  permissions :update? do
-    pending "add some examples to (or delete) #{__FILE__}"
-  end
+  context "Scope" do
+    it "should only return person certificates for the people that are on the current product" do
+      ActsAsTenant.without_tenant do
+        current_product = create(:product)
+        another_product = create(:product)
+        person = create(:person, role: :admin, product_id: current_product.id)
+        person2 = create(:person, role: :admin, product_id: another_product.id)
 
-  permissions :destroy? do
-    pending "add some examples to (or delete) #{__FILE__}"
+        certificate = create(:certificate, product_id: current_product.id)
+        certificate2 = create(:certificate, product_id: current_product.id)
+        certificate3 = create(:certificate, product_id: another_product.id)
+        person_certificate = create(:person_certificate, certificate_id: certificate.id, person_id: person.id)
+        person_certificate2 = create(:person_certificate, certificate_id: certificate2.id, person_id: person.id)
+        create(:person_certificate, certificate_id: certificate3.id, person_id: person2.id)
+        expect(PersonCertificate.count).to eq(3) # to test if all the person certificates pages are created
+
+        ActsAsTenant.current_tenant = current_product
+        scope = Pundit.policy_scope!(person, PersonCertificate.all)
+        expect(scope.count).to eq(2)
+        expect(scope).to include(person_certificate)
+        expect(scope).to include(person_certificate2)
+      end
+    end
   end
 end
--- spec/policies/person_poll_option_policy_spec.rb
+++ spec/policies/person_poll_option_policy_spec.rb
@@ -1,27 +1,77 @@
-require 'rails_helper'
+require "rails_helper"
 
 RSpec.describe PersonPollOptionPolicy, type: :policy do
-  let(:user) { User.new }
+  subject { described_class.new(person, person_poll_option) }
 
-  subject { described_class }
+  let(:person) { nil }
 
-  permissions ".scope" do
-    pending "add some examples to (or delete) #{__FILE__}"
+  context "CRUD actions" do
+    let(:person_poll_option) { PersonPollOption.new }
+
+    it { is_expected.to permit_actions(%i[index show ]) }
+    it { is_expected.to forbid_actions(%i[new create update edit destroy]) }
   end
 
-  permissions :show? do
-    pending "add some examples to (or delete) #{__FILE__}"
+  context "Rails admin actions" do
+    let(:person_poll_option) { PersonPollOption.new }
+
+    it { is_expected.to permit_actions(%i[export history dashboard select_product_dashboard]) }
+    it { is_expected.to forbid_actions(%i[show_in_app generate_game_action]) }
   end
 
-  permissions :create? do
-    pending "add some examples to (or delete) #{__FILE__}"
+  describe "#select_product" do
+    let(:person_poll_option) { PersonPollOption.new }
+
+    context "superadmin who has the admin product assigned" do
+      let(:product) { create(:product, internal_name: "admin") }
+      let(:person) { Person.new(product: product, role: :super_admin) }
+
+      it { is_expected.to permit_action(:select_product) }
+    end
+
+    context "admin who has the admin product assigned" do
+      let(:product) { create(:product, internal_name: "admin") }
+      let(:person) { Person.new(product: product, role: :admin) }
+
+      it { is_expected.to forbid_action(:select_product) }
+    end
+
+    context "superadmin who doesn't have admin product assigned" do
+      let(:product) { create(:product, internal_name: "not_admin") }
+      let(:person) { Person.new(product: product, role: :super_admin) }
+
+      it { is_expected.to forbid_action(:select_product) }
+    end
   end
 
-  permissions :update? do
-    pending "add some examples to (or delete) #{__FILE__}"
-  end
+  context "Scope" do
+    it "should only return person certificates for the people that are on the current product" do
+      ActsAsTenant.without_tenant do
+        current_product = create(:product)
+        another_product = create(:product)
+        person = create(:person, role: :admin, product_id: current_product.id)
+        person2 = create(:person, role: :admin, product_id: another_product.id)
 
-  permissions :destroy? do
-    pending "add some examples to (or delete) #{__FILE__}"
+        poll = create(:poll, product_id: current_product.id)
+        poll2 = create(:poll, product_id: another_product.id)
+
+        poll_option = create(:poll_option, poll_id: poll.id)
+        poll_option2 = create(:poll_option, poll_id: poll.id)
+        poll_option3 = create(:poll_option, poll_id: poll2.id)
+
+        person_poll_option = create(:person_poll_option, poll_option_id: poll_option.id, person_id: person.id)
+        person_poll_option2 = create(:person_poll_option,  poll_option_id: poll_option2.id, person_id: person.id)
+        create(:person_poll_option,  poll_option_id: poll_option3.id, person_id: person2.id)
+
+        expect(PersonPollOption.count).to eq(3) # to test if all the person poll options are created
+
+        ActsAsTenant.current_tenant = current_product
+        scope = Pundit.policy_scope!(person, PersonPollOption.all)
+
+        expect(scope.count).to eq(2)
+        expect(scope).to include(person_poll_option)
+        expect(scope).to include(person_poll_option2)
+      end
+    end
   end
 end
