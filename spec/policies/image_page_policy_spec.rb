055f3092704420252013c4a2dd406bfac0d4fdee
Cristian Somesan <crs@flink.to>
2019-08-27 15:21:45 +0300

    Add policy tests for image page

--- spec/policies/image_page_policy_spec.rb
+++ spec/policies/image_page_policy_spec.rb
@@ -1,27 +1,70 @@
-require 'rails_helper'
+require "rails_helper"
 
 RSpec.describe ImagePagePolicy, type: :policy do
-  let(:user) { User.new }
+  subject { described_class.new(person, certificate) }
 
-  subject { described_class }
+  let(:person) { nil }
 
-  permissions ".scope" do
-    pending "add some examples to (or delete) #{__FILE__}"
+  context "CRUD actions" do
+    let(:certificate) { Certificate.new }
+
+    it { is_expected.to permit_new_and_create_actions }
+    it { is_expected.to permit_edit_and_update_actions }
+    it { is_expected.to forbid_action(:destroy) }
+    it { is_expected.to permit_action(:index) }
+    it { is_expected.to permit_action(:show) }
   end
 
-  permissions :show? do
-    pending "add some examples to (or delete) #{__FILE__}"
+  context "Rails admin actions" do
+    let(:certificate) { Certificate.new }
+
+    it { is_expected.to permit_actions(%i[export history dashboard select_product_dashboard]) }
+    it { is_expected.to forbid_actions(%i[show_in_app generate_game_action]) }
   end
 
-  permissions :create? do
-    pending "add some examples to (or delete) #{__FILE__}"
+  describe "#select_product" do
+    let(:certificate) { Certificate.new }
+
+    context "superadmin who has the admin product assigned" do
+      let(:product) { create(:product, internal_name: "admin") }
+      let(:person) { Person.new(product: product, role: :super_admin) }
+
+      it { is_expected.to permit_action(:select_product) }
+    end
+
+    context "admin who has the admin product assigned" do
+      let(:product) { create(:product, internal_name: "admin") }
+      let(:person) { Person.new(product: product, role: :admin) }
+
+      it { is_expected.to forbid_action(:select_product) }
+    end
+
+    context "superadmin who doesn't have admin product assigned" do
+      let(:product) { create(:product, internal_name: "not_admin") }
+      let(:person) { Person.new(product: product, role: :super_admin) }
+
+      it { is_expected.to forbid_action(:select_product) }
+    end
   end
 
-  permissions :update? do
-    pending "add some examples to (or delete) #{__FILE__}"
-  end
+  context "Scope" do
+    it "should only return certcourses pages from the current product" do
+      ActsAsTenant.without_tenant do
+        person = create(:person, role: :admin)
+        current_product = create(:product)
+        another_product = create(:product)
 
-  permissions :destroy? do
-    pending "add some examples to (or delete) #{__FILE__}"
+        certificate = create(:certificate, product_id: current_product.id)
+        certificate2 = create(:certificate, product_id: current_product.id)
+        create(:certificate, product_id: another_product.id)
+        expect(Certificate.count).to eq(3) # to test if all the certcourses pages are created
+
+        ActsAsTenant.current_tenant = current_product
+        scope = Pundit.policy_scope!(person, Certificate.all)
+        expect(scope.count).to eq(2)
+        expect(scope).to include(certificate)
+        expect(scope).to include(certificate2)
+      end
+    end
   end
 end
