cdec6682595bc69c6cfc656ef2f553722944156b
Cristian Somesan <crs@flink.to>
2019-08-28 11:26:53 +0300

    Add policy test for person quiz and portal access

--- spec/policies/person_quiz_policy_spec.rb
+++ spec/policies/person_quiz_policy_spec.rb
@@ -1,27 +1,74 @@
-require 'rails_helper'
+require "rails_helper"
 
 RSpec.describe PersonQuizPolicy, type: :policy do
-  let(:user) { User.new }
+  subject { described_class.new(person, person_quiz) }
 
-  subject { described_class }
+  let(:person) { nil }
 
-  permissions ".scope" do
-    pending "add some examples to (or delete) #{__FILE__}"
+  context "CRUD actions" do
+    let(:person_quiz) { PersonQuiz.new }
+
+    it { is_expected.to permit_new_and_create_actions }
+    it { is_expected.to permit_edit_and_update_actions }
+    it { is_expected.to forbid_action(:destroy) }
+    it { is_expected.to permit_action(:index) }
+    it { is_expected.to permit_action(:show) }
   end
 
-  permissions :show? do
-    pending "add some examples to (or delete) #{__FILE__}"
+  context "Rails admin actions" do
+    let(:person_quiz) { PersonQuiz.new }
+
+    it { is_expected.to permit_actions(%i[export history dashboard select_product_dashboard]) }
+    it { is_expected.to forbid_actions(%i[show_in_app generate_game_action]) }
   end
 
-  permissions :create? do
-    pending "add some examples to (or delete) #{__FILE__}"
+  describe "#select_product" do
+    let(:person_quiz) { PersonQuiz.new }
+
+    context "superadmin who has the admin product assigned" do
+      let(:product) { create(:product, internal_name: "admin") }
+      let(:person) { Person.new(product: product, role: :super_admin) }
+
+      it { is_expected.to permit_action(:select_product) }
+    end
+
+    context "admin who has the admin product assigned" do
+      let(:product) { create(:product, internal_name: "admin") }
+      let(:person) { Person.new(product: product, role: :admin) }
+
+      it { is_expected.to forbid_action(:select_product) }
+    end
+
+    context "superadmin who doesn't have admin product assigned" do
+      let(:product) { create(:product, internal_name: "not_admin") }
+      let(:person) { Person.new(product: product, role: :super_admin) }
+
+      it { is_expected.to forbid_action(:select_product) }
+    end
   end
 
-  permissions :update? do
-    pending "add some examples to (or delete) #{__FILE__}"
-  end
+  context "Scope" do
+    it "should only return person quizzes for the people that are on the current product" do
+      ActsAsTenant.without_tenant do
+        current_product = create(:product)
+        another_product = create(:product)
+        person = create(:person, role: :admin, product_id: current_product.id)
+        person2 = create(:person, role: :admin, product_id: another_product.id)
 
-  permissions :destroy? do
-    pending "add some examples to (or delete) #{__FILE__}"
+        quiz_page = create(:quiz_page, product_id: current_product.id)
+        quiz_page2 = create(:quiz_page, product_id: current_product.id)
+        quiz_page3 = create(:quiz_page, product_id: another_product.id)
+        person_quiz = create(:person_quiz, quiz_page_id: quiz_page.id, person_id: person.id)
+        person_quiz2 = create(:person_quiz, quiz_page_id: quiz_page2.id, person_id: person.id)
+        create(:person_quiz, quiz_page_id: quiz_page3.id, person_id: person2.id)
+        expect(PersonQuiz.count).to eq(3) # to test if all the person certificates pages are created
+
+        ActsAsTenant.current_tenant = current_product
+        scope = Pundit.policy_scope!(person, PersonQuiz.all)
+        expect(scope.count).to eq(2)
+        expect(scope).to include(person_quiz)
+        expect(scope).to include(person_quiz2)
+      end
+    end
   end
 end
--- spec/policies/portal_access_policy_spec.rb
+++ spec/policies/portal_access_policy_spec.rb
@@ -1,27 +1,72 @@
 require "rails_helper"
 
 RSpec.describe PortalAccessPolicy, type: :policy do
-  let(:user) { User.new }
+  subject { described_class.new(person, portal_access) }
 
-  subject { described_class }
+  let(:person) { nil }
 
-  permissions ".scope" do
-    pending "add some examples to (or delete) #{__FILE__}"
+  context "CRUD actions" do
+    let(:portal_access) { PortalAccess.new }
+
+    it { is_expected.to permit_new_and_create_actions }
+    it { is_expected.to permit_edit_and_update_actions }
+    it { is_expected.to forbid_action(:destroy) }
+    it { is_expected.to permit_action(:index) }
+    it { is_expected.to permit_action(:show) }
   end
 
-  permissions :show? do
-    pending "add some examples to (or delete) #{__FILE__}"
+  context "Rails admin actions" do
+    let(:portal_access) { PortalAccess.new }
+
+    it { is_expected.to permit_actions(%i[export history dashboard select_product_dashboard]) }
+    it { is_expected.to forbid_actions(%i[show_in_app generate_game_action]) }
   end
 
-  permissions :create? do
-    pending "add some examples to (or delete) #{__FILE__}"
+  describe "#select_product" do
+    let(:portal_access) { PortalAccess.new }
+
+    context "superadmin who has the admin product assigned" do
+      let(:product) { create(:product, internal_name: "admin") }
+      let(:person) { Person.new(product: product, role: :super_admin) }
+
+      it { is_expected.to permit_action(:select_product) }
+    end
+
+    context "admin who has the admin product assigned" do
+      let(:product) { create(:product, internal_name: "admin") }
+      let(:person) { Person.new(product: product, role: :admin) }
+
+      it { is_expected.to forbid_action(:select_product) }
+    end
+
+    context "superadmin who doesn't have admin product assigned" do
+      let(:product) { create(:product, internal_name: "not_admin") }
+      let(:person) { Person.new(product: product, role: :super_admin) }
+
+      it { is_expected.to forbid_action(:select_product) }
+    end
   end
 
-  permissions :update? do
-    pending "add some examples to (or delete) #{__FILE__}"
-  end
+  context "Scope" do
+    it "should only return person quizzes for the people that are on the current product" do
+      ActsAsTenant.without_tenant do
+        binding.pry
+        current_product = create(:product)
+        another_product = create(:product)
+        person = create(:person, role: :admin, product_id: current_product.id)
+        person2 = create(:person, role: :admin, product_id: another_product.id)
 
-  permissions :destroy? do
-    pending "add some examples to (or delete) #{__FILE__}"
+        portal_access = create(:portal_access, person_id: person.id)
+        portal_access2 = create(:portal_access, person_id: person.id)
+        create(:portal_access, person_id: person2.id)
+        expect(PortalAccess.count).to eq(3) # to test if all the person certificates pages are created
+
+        ActsAsTenant.current_tenant = current_product
+        scope = Pundit.policy_scope!(person, PortalAccess.all)
+        expect(scope.count).to eq(2)
+        expect(scope).to include(portal_access)
+        expect(scope).to include(portal_access2)
+      end
+    end
   end
 end
