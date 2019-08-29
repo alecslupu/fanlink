038a563b5aebe8f7a208a0105a9ba7065c5abcb2
Cristian Somesan <crs@flink.to>
2019-08-29 11:50:48 +0300

    Add category, certcourse and certcurse page policy tests

--- spec/policies/certcourse_policy_spec.rb
+++ spec/policies/certcourse_policy_spec.rb
@@ -1,69 +1,94 @@
+# frozen_string_literal: true
+
 require "rails_helper"
 
 RSpec.describe CertcoursePolicy, type: :policy do
-  subject { described_class.new(person, certcourse) }
+  permission_list = {
+    index: false,
+    show: false,
+    create: false,
+    new: false,
+    update: false,
+    edit: false,
+    destroy: false,
+    export: false,
+    history: false,
+    show_in_app: false,
+    dashboard: false,
+    # select_product_dashboard: false,
+    select_product: false,
+  }
 
-  let(:person) { nil }
+  describe "defined policies" do
+    subject { described_class.new(Person.new, Certcourse.new) }
 
-  context "CRUD actions" do
-    let(:certcourse) { Certcourse.new }
-
-    it { is_expected.to permit_new_and_create_actions }
-    it { is_expected.to permit_edit_and_update_actions }
-    it { is_expected.to forbid_action(:destroy) }
-    it { is_expected.to permit_action(:index) }
-    it { is_expected.to permit_action(:show) }
+    permission_list.each do |policy, value|
+      it { is_expected.to respond_to("#{policy}?".to_sym) }
+    end
   end
 
-  context "Rails admin actions" do
-    let(:certcourse) { Certcourse.new }
+  context "logged out user" do
+    subject { described_class.new(nil, Certcourse.new) }
 
-    it { is_expected.to permit_actions(%i[export history dashboard select_product_dashboard]) }
-    it { is_expected.to forbid_actions(%i[show_in_app generate_game_action]) }
+    describe "permissions" do
+      permission_list.each do |policy, value|
+        it { is_expected.to forbid_action(policy) }
+      end
+    end
+    describe "protected methods" do
+      it do
+        expect(subject.send(:module_name)).to eq("courseware")
+      end
+      it do
+        expect(subject.send(:super_admin?)).to be_nil
+      end
+      it do
+        expect(subject.send(:has_permission?, "bogous")).to eq(false)
+      end
+    end
   end
 
-  describe "#select_product" do
-    let(:certcourse) { Certcourse.new }
+  context "user with super admin role and with admin product" do
+    let(:product) { create(:product, internal_name: "admin") }
 
-    context "superadmin who has the admin product assigned" do
-      let(:product) { create(:product, internal_name: "admin") }
-      let(:person) { Person.new(product: product, role: :super_admin) }
+    subject { described_class.new(Person.new(role: :super_admin, product: product), Certcourse.new) }
 
-      it { is_expected.to permit_action(:select_product) }
+    describe "permissions" do
+      permission_list.each do |policy, _|
+        it { is_expected.to permit_action(policy) } unless policy == :show_in_app
+      end
+
+      it { is_expected.to forbid_action(:show_in_app) }
     end
 
-    context "admin who has the admin product assigned" do
-      let(:product) { create(:product, internal_name: "admin") }
-      let(:person) { Person.new(product: product, role: :admin) }
-
-      it { is_expected.to forbid_action(:select_product) }
-    end
-
-    context "superadmin who doesn't have admin product assigned" do
-      let(:product) { create(:product, internal_name: "not_admin") }
-      let(:person) { Person.new(product: product, role: :super_admin) }
-
-      it { is_expected.to forbid_action(:select_product) }
+    describe "protected methods" do
+      it do
+        expect(subject.send(:module_name)).to eq("courseware")
+      end
+      it do
+        expect(subject.send(:super_admin?)).to eq(true)
+      end
+      it do
+        expect(subject.send(:has_permission?, "bogous")).to eq(true)
+      end
     end
   end
 
   context "Scope" do
-    it "should only return certcourses pages from the current product" do
-      ActsAsTenant.without_tenant do
-        person = create(:person, role: :admin)
-        current_product = create(:product)
-        another_product = create(:product)
+    it "should return all the assigned_rewards" do
+      person = create(:person)
 
-        certcourse = create(:certcourse, product_id: current_product.id)
-        certcourse2 = create(:certcourse, product_id: current_product.id)
-        create(:certcourse, product_id: another_product.id)
-        expect(Certcourse.count).to eq(3) # to test if all the certcourses pages are created
+      ActsAsTenant.with_tenant(person.product) do
 
-        ActsAsTenant.current_tenant = current_product
+        certcourse = create(:certcourse)
+        certcourse = create(:certcourse)
+
+        expect(Certcourse.count).to eq(2)
+
         scope = Pundit.policy_scope!(person, Certcourse.all)
         expect(scope.count).to eq(2)
         expect(scope).to include(certcourse)
-        expect(scope).to include(certcourse2)
+        expect(scope).to include(certcourse)
       end
     end
   end
