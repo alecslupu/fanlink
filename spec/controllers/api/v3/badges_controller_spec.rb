56f2139b13acca5ee5d1970a8a7e11244b19e88d
Cristian Somesan <crs@flink.to>
2019-09-02 19:05:12 +0300

    More badges attachment specs

--- spec/controllers/api/v3/badges_controller_spec.rb
+++ spec/controllers/api/v3/badges_controller_spec.rb
@@ -2,9 +2,21 @@ require "rails_helper"
 
 RSpec.describe Api::V3::BadgesController, type: :controller do
 
-  # TODO: auto-generated
   describe "GET index" do
-    pending
+    it "should return all badges attachment with their images" do
+      person = create(:person, role: :admin)
+      ActsAsTenant.with_tenant(person.product) do
+        login_as(person)
+        create_list(:badge,3)
+        get :index
+        parsed_response = JSON.parse(response.body)
+        expect(response).to have_http_status(200)
+        expect(parsed_response["badges"].size).to eq(3)
+        parsed_response["badges"].each do |badge|
+          expect(Badge.first.picture.exists?).to eq(true)
+        end
+      end
+    end
   end
 
   describe "POST create" do
@@ -52,13 +64,22 @@ RSpec.describe Api::V3::BadgesController, type: :controller do
     end
   end
 
-  # TODO: auto-generated
   describe "GET show" do
-    pending
+    it "should return all badges attachment with their images" do
+      person = create(:person, role: :admin)
+      ActsAsTenant.with_tenant(person.product) do
+        login_as(person)
+        badge = create(:badge)
+        get :show, params: { id: badge.id }
+        parsed_response = JSON.parse(response.body)
+        expect(response).to have_http_status(200)
+        expect(Badge.first.picture.exists?).to eq(true)
+      end
+    end
   end
 
-  # TODO: auto-generated
-  describe "DELETE destroy" do
-    pending
-  end
+  # # TODO: auto-generated
+  # describe "DELETE destroy" do
+  #   pending
+  # end
 end
--- spec/controllers/api/v4/badges_controller_spec.rb
+++ spec/controllers/api/v4/badges_controller_spec.rb
@@ -2,9 +2,21 @@ require "rails_helper"
 
 RSpec.describe Api::V4::BadgesController, type: :controller do
 
-  # TODO: auto-generated
   describe "GET index" do
-    pending
+    it "should return all badges attachment with their images" do
+      person = create(:person, role: :admin)
+      ActsAsTenant.with_tenant(person.product) do
+        login_as(person)
+        create_list(:badge,3)
+        get :index
+        parsed_response = JSON.parse(response.body)
+        expect(response).to have_http_status(200)
+        expect(parsed_response["badges"].size).to eq(3)
+        parsed_response["badges"].each do |badge|
+          expect(Badge.first.picture.exists?).to eq(true)
+        end
+      end
+    end
   end
 
   describe "POST create" do
@@ -48,6 +60,20 @@ RSpec.describe Api::V4::BadgesController, type: :controller do
         expect(response).to have_http_status(200)
         expect(Badge.first.picture.exists?).to eq(true)
         expect(parsed_response["badge"]["picture_url"]).to include("better.png")
+      end
+    end
+  end
+
+  describe "GET show" do
+    it "should return all badges attachment with their images" do
+      person = create(:person, role: :admin)
+      ActsAsTenant.with_tenant(person.product) do
+        login_as(person)
+        badge = create(:badge)
+        get :show, params: { id: badge.id }
+        parsed_response = JSON.parse(response.body)
+        expect(response).to have_http_status(200)
+        expect(Badge.first.picture.exists?).to eq(true)
       end
     end
   end
