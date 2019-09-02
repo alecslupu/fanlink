e28741311c5224caef25ad1c36b1d2b24ea6663c
Cristian Somesan <crs@flink.to>
2019-09-02 16:22:57 +0300

    Add controller test for badges create action

--- spec/controllers/api/v3/badges_controller_spec.rb
+++ spec/controllers/api/v3/badges_controller_spec.rb
@@ -9,7 +9,24 @@ RSpec.describe Api::V3::BadgesController, type: :controller do
 
   # TODO: auto-generated
   describe "POST create" do
-    pending
+    it "should create a badge with attachment when it's valid" do
+      person = create(:person, role: :admin)
+      ActsAsTenant.with_tenant(person.product) do
+        login_as(person)
+        post :create, params: {
+          badge: {
+            product_id: person.product.id,
+            action_type_id: create(:action_type).id,
+            internal_name: "act1",
+            picture: fixture_file_upload("images/better.png", "image/png")
+          }
+        }
+        parsed_response = JSON.parse(response.body)
+        expect(response).to have_http_status(200)
+        expect(Badge.first.picture.exists?).to eq(true)
+        expect(parsed_response["badge"]["picture_url"]).to include("better.png")
+      end
+    end
   end
 
   # TODO: auto-generated
