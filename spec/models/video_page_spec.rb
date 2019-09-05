a0915183f9ef9fccbee7f17b8f7f982e10b4c892
Cristian Somesan <crs@flink.to>
2019-09-05 16:02:33 +0300

    Add test for video duration

--- spec/models/video_page_spec.rb
+++ spec/models/video_page_spec.rb
@@ -1,8 +1,26 @@
-require "rails_helper"
+require 'rails_helper'
 
 RSpec.describe VideoPage, type: :model do
-  describe "#product" do
+  describe '#product' do
     pending
   end
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  describe '#set_certcourse_page_duration' do
+    let(:certcourse_page) { create(:certcourse_page, duration: 300) }
+
+    it 'changes the duration of the page' do
+      video_page = create(
+        :video_page,
+        certcourse_page: certcourse_page,
+        product: Product.find(certcourse_page.product_id),
+        video: fixture_file_upload(
+          Rails.root.join('spec', 'fixtures', 'videos', 'short_video.mp4')
+        )
+      )
+      duration = FFMPEG::Movie.new(
+        Paperclip.io_adapters.for(video_page.video).path
+      ).duration.to_i + 1
+      expect(certcourse_page.duration).to eq(duration)
+    end
+  end
 end
