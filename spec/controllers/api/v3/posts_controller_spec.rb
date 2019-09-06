64224dc3bac469973c293d8294ff6e9135890bf2
Cristian Somesan <crs@flink.to>
2019-09-06 16:58:20 +0300

    Add paperclip tests for posts

--- spec/controllers/api/v3/posts_controller_spec.rb
+++ spec/controllers/api/v3/posts_controller_spec.rb
@@ -109,6 +109,29 @@ RSpec.describe Api::V3::PostsController, type: :controller do
         expect(response).to be_unauthorized
       end
     end
+
+    it "creates a post with attachments when it's valid" do
+      person = create(:person, role: :admin)
+      ActsAsTenant.with_tenant(person.product) do
+        login_as(person)
+
+        post :create, params: {
+          post: {
+            body: "Body",
+            picture: fixture_file_upload('images/better.png', 'image/png'),
+            audio: fixture_file_upload('audio/small_audio.mp4', 'audio/mp4'),
+            video: fixture_file_upload('videos/short_video.mp4', 'video/mp4')
+          }
+        }
+
+        expect(response).to have_http_status(200)
+        expect(Post.last.picture.exists?).to be_truthy
+        expect(Post.last.audio.exists?).to be_truthy
+        expect(json['post']['picture_url']).to include('better.png')
+        expect(json['post']['audio_url']).to include('small_audio')
+        expect(json['post']['video_url']).to include('short_video')
+      end
+    end
   end
 
   describe "#destroy" do
