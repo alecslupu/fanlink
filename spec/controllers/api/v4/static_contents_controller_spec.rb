55b1c2b74f480ecb1e11da9e8097ecc15518b36a
Cristian Somesan <crs@flink.to>
2019-09-11 09:53:35 +0300

    Rubocop changes

--- spec/controllers/api/v4/static_contents_controller_spec.rb
+++ spec/controllers/api/v4/static_contents_controller_spec.rb
@@ -8,7 +8,10 @@ RSpec.describe Api::V4::StaticContentsController, type: :controller do
         static_content = create(:static_content)
         login_as(person)
 
-        get :show, params: { product_id: static_content.product.id, slug: static_content.slug }
+        get :show, params: {
+          product_id: static_content.product.id,
+          slug: static_content.slug
+        }
 
         expect(response).to be_successful
         expect(static_content_json(json)).to be_truthy
