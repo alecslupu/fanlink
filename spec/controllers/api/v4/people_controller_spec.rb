07ca445b94b25e7bfe92312da345b76a81658411
Cristian Somesan <crs@flink.to>
2019-09-26 16:32:09 +0300

    Add tests

--- spec/controllers/api/v4/people_controller_spec.rb
+++ spec/controllers/api/v4/people_controller_spec.rb
@@ -2,6 +2,19 @@ require "rails_helper"
 
 RSpec.describe Api::V4::PeopleController, type: :controller do
 
+  describe "#change_password" do
+    it "should not change the current users password if it matches the current one" do
+      password = "password"
+      person = create(:person, password: password)
+      ActsAsTenant.with_tenant(person.product) do
+        login_as(person)
+        patch :change_password,  params: { id: person.id, person: { current_password: password, new_password: password } }
+        expect(response).to be_unprocessable
+        expect(json["errors"]).to include("New password can't be identical to the current one")
+      end
+    end
+  end
+
   # TODO: auto-generated
   describe "GET index" do
     pending
