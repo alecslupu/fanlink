9639ef258eba420a3d91bbf3772b233fec7ddb32
Cristian Somesan <crs@flink.to>
2019-09-27 11:34:54 +0300

    Add tests for dependencies

--- spec/models/certcourse_page_spec.rb
+++ spec/models/certcourse_page_spec.rb
@@ -6,6 +6,45 @@ RSpec.describe CertcoursePage, type: :model do
     it { expect(create(:certcourse_page)).to be_valid }
   end
 
+  describe "dependencies" do
+    it "destroys dependent quiz page" do
+      certcourse_page = create(:certcourse_page)
+      quiz_page = create(:quiz_page, certcourse_page: certcourse_page)
+      no_of_quiz_answers = quiz_page.answers.count
+
+      expect { certcourse_page.destroy }.to change { QuizPage.count }.by(-1)
+    end
+
+    it "destroys dependent video page" do
+      video_page = create(:video_page)
+
+      expect { video_page.certcourse_page.destroy }.to change { VideoPage.count }.by(-1)
+    end
+
+    it "destroys dependent image page" do
+      image_page = create(:image_page)
+
+      expect { image_page.certcourse_page.destroy }.to change { ImagePage.count }.by(-1)
+    end
+
+    it "destroys dependent download file page" do
+      download_file_page = create(:download_file_page)
+
+      expect { download_file_page.certcourse_page.destroy }.to change { DownloadFilePage.count }.by(-1)
+    end
+
+    it "destroys dependent course page progresses" do
+      certcourse_page = create(:certcourse_page)
+      course_page_progress = create(
+        :course_page_progress,
+        certcourse_page_id: certcourse_page.id,
+        person: create(:person)
+      )
+
+      expect { course_page_progress.destroy }.to change { CoursePageProgress.count }.by(-1)
+    end
+  end
+
   # TODO: auto-generated
   describe "#content_type" do
     pending
@@ -22,5 +61,4 @@ RSpec.describe CertcoursePage, type: :model do
   end
 
 
-  pending "add some examples to (or delete) #{__FILE__}"
 end
