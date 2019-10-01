94ad994cc073d169a069558b347afd17ffeab017
Alexandru-Emil Lupu <contact@alecslupu.ro>
2019-10-02 00:15:28 +0300

    cleanup

---
+++ old/spec/spec_old2/generated/controllers/admin/portal_accesses_controller_spec.rb
@@ -0,0 +1,15 @@
+# -*- encoding: utf-8 -*-
+
+require 'rails_helper'
+
+describe Admin::PortalAccessesController do
+
+  # TODO: auto-generated
+  describe 'GET new' do
+    it 'works' do
+      get :new, {}, {}
+      expect(response.status).to eq(200)
+    end
+  end
+
+end
