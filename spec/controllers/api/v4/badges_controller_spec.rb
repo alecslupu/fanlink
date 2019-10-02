297aa10c13a2cf01f8bbaa629e23ccf31b42e0a4
Cristian Somesan <crs@flink.to>
2019-10-02 19:22:28 +0300

    Add test to verify

--- spec/controllers/api/v4/badges_controller_spec.rb
+++ spec/controllers/api/v4/badges_controller_spec.rb
@@ -1,24 +1,29 @@
 require "rails_helper"
 
 RSpec.describe Api::V4::BadgesController, type: :controller do
-
-  # TODO: auto-generated
-  describe "GET index" do
-    pending
-  end
-
-  # TODO: auto-generated
-  describe "POST create" do
-    pending
-  end
-
-  # TODO: auto-generated
-  describe "PUT update" do
-    pending
-  end
-
-  # TODO: auto-generated
-  describe "GET show" do
-    pending
+  describe 'POST create' do
+    it 'creates a badge' do
+      user = create(:person, role: :admin)
+      ActsAsTenant.with_tenant(user.product) do
+        login_as(user)
+        name = 'A name'
+        internal = 'internal'
+        description = 'description'
+        action_type = create(:action_type)
+        post :create, params: {
+          badge: {
+            name: name,
+            internal_name: internal,
+            description: description,
+            action_type_id: action_type.id
+          }
+        }
+        expect(response).to be_successful
+        badge = Badge.last
+        expect(badge.name).to eq(name)
+        expect(badge.internal_name).to eq(internal_name)
+        expect(badge.action_type_id).to eq(action_type.id)
+      end
+    end
   end
 end
