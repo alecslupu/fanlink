a3cd6ff0d3de3470a513e1f6c0fd7f9606bcc3be
Cristian Somesan <crs@flink.to>
2019-10-03 19:16:04 +0300

    Add one more test

--- spec/controllers/api/v4/badge_actions_controller_spec.rb
+++ spec/controllers/api/v4/badge_actions_controller_spec.rb
@@ -1,23 +1,35 @@
-require "rails_helper"
+require 'rails_helper'
 
 RSpec.describe Api::V4::BadgeActionsController, type: :controller do
 
-  describe "POST create" do
-    it "return an error when the are no rewards on the badges" do
+  describe 'POST create' do
+    it 'return an error when the are no rewards on the badges' do
       person = create(:person)
       ActsAsTenant.with_tenant(person.product) do
         login_as(person)
         action_type = create(:action_type)
-        badge1 = create(:badge, action_type: action_type, action_requirement: 3)
         badge2 = create(:badge, action_type: action_type, action_requirement: 4)
-        badge_other = create(:badge)
-        binding.pry
+        badge2.reward.destroy
         post :create, params: { badge_action: { action_type: action_type.internal_name } }
-        expect(response).to have_http_status(422)
-        expect(json["errors"].first).to include(_('The are no rewards assigned for the badges on this action type'))
-        # expect(pending_badge_json(json["pending_badge"], badge1)).to be_truthy
+
+        expect(response).to be_not_found
+        expect(json['errors']['base']).to include(_('Reward does not exist for that action type.'))
+      end
+    end
+
+    it 'the awarded badge' do
+      person = create(:person)
+      ActsAsTenant.with_tenant(person.product) do
+        login_as(person)
+        action_type = create(:action_type)
+        badge = create(:badge, action_type: action_type)
+        badge2 = create(:badge, action_type: action_type)
+        reward = badge.reward
+        reward2 = badge2.reward
+
+        post :create, params: { badge_action: { action_type: action_type.internal_name, identifier: 345 } }
+        expect(json["badges_awarded"]). to be_truthy
       end
     end
   end
-
 end
