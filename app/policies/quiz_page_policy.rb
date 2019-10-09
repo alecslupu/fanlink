9ec290454f0736f5572b655b4c216bbc6072ac7d
Cristian Somesan <crs@flink.to>
2019-10-09 09:37:41 +0000

    quiz_page_policy.rb edited online with Bitbucket

--- app/policies/quiz_page_policy.rb
+++ app/policies/quiz_page_policy.rb
@@ -1,4 +1,4 @@
-class QuizPagePolicy < ApplicationPolicy
+class QuizPagePolicy < CoursewarePolicy
   class Scope < Scope
     def resolve
       super.for_product(ActsAsTenant.current_tenant).includes(certcourse_page: :certcourse)
