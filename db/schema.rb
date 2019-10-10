776606f23a69fe804971e781d4b2b0dc3b323d43
Cristian Somesan <crs@flink.to>
2019-10-10 11:59:30 +0300

    Add tests

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190930132129) do
+ActiveRecord::Schema.define(version: 20191009160601) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -1047,6 +1047,7 @@ ActiveRecord::Schema.define(version: 20190930132129) do
     t.jsonb "name", default: {}, null: false
     t.jsonb "description", default: {}, null: false
     t.integer "order", default: 0, null: false
+    t.bigint "last_message_timestamp"
     t.index ["created_by_id"], name: "index_rooms_on_created_by_id"
     t.index ["product_id", "status"], name: "unq_rooms_product_status"
   end
--- spec/controllers/api/v4/messages_controller_spec.rb
+++ spec/controllers/api/v4/messages_controller_spec.rb
@@ -31,12 +31,45 @@ RSpec.describe Api::V4::MessagesController, type: :controller do
 
   # TODO: auto-generated
   describe "POST create" do
-    pending
+    it "creates a new message in a public room and updates the timestamp" do
+      person = create(:person)
+      ActsAsTenant.with_tenant(person.product) do
+        login_as(person)
+        body = "Do you like my body?"
+        room = create(:room, public: true, status: :active)
+
+        expect(room.last_message_timestamp).to eq(nil)
+
+        timestamp = DateTime.now.to_i
+        post :create, params: { room_id: room.id,  message: { body: body } }
+        expect(response).to be_successful
+
+        msg = Message.last
+
+        expect(msg.room).to eq(room)
+        expect(msg.person).to eq(person)
+        expect(msg.body).to eq(body)
+        expect(json["message"]["body"]).to eq(body)
+        expect(room.reload.last_message_timestamp).to be >= timestamp
+      end
+    end
   end
 
   # TODO: auto-generated
   describe "PUT update" do
-    pending
+    it "does not update the timestamp" do
+      admin_person = create(:admin_user)
+      ActsAsTenant.with_tenant(admin_person.product) do
+        login_as(admin_person)
+        room = create(:room, public: true, status: :active, last_message_timestamp: 1)
+        msg = create(:message, room: room)
+
+        expect {
+          patch :update, params: { id: msg.id, message: { hidden: true } }
+        }.not_to change { room.last_message_timestamp }
+
+      end
+    end
   end
 
   # TODO: auto-generated
