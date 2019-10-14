677b3295406bf39fe3d22bee5c79b85eed41c4b7
Cristian Somesan <crs@flink.to>
2019-10-14 12:04:38 +0300

    Refactor post share + add locals

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,6 +3,20 @@ class PostsController < ApplicationController
 
   def share
     product =  Product.find_by(internal_name: params[:product])
-    @post = Post.for_product(product).visible.find(params[:post_id])
+    post = Post.for_product(product).visible.find(params[:post_id])
+    @locals = generate_locals(post)
+  end
+
+  private
+
+  def generate_locals(post)
+    {
+      body: post.body.present? ? post.body.html_safe : "",
+      title: "Check out this post on #{post.product.name}!",
+      post_image_url: post.picture.exists? ? post.picture.url : "https://fanlinkmusic.com/static/img/pattern-2.487c258.jpg",
+      # post_url: "#{root_url}/download/#{product.internal_name}",
+      user_image_url: post.person.picture.exists? ? post.person.picture.url : "https://fanlinkmusic.com/static/img/pattern-2.487c258.jpg",
+      username: post.person.username
+    }
   end
 end
