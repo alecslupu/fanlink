e275198fccb579769bd3cbb76cbee9f3d267ae49
Cristian Somesan <crs@flink.to>
2019-10-18 15:07:51 +0300

    More setup

--- app/controllers/api/v4/notifications_controller.rb
+++ app/controllers/api/v4/notifications_controller.rb
@@ -1,14 +1,18 @@
-# class Api::V4::NotificationsController < ApiController
-# def notify
-#   if current_user.pin_messages_from || current_user.product_account
-#     receipents =
-#   else
-#     return render_401 _("You are not allowed to send notifications.")
-#   end
+class Api::V4::NotificationsController < ApiController
+  def create
+    if current_user.pin_messages_from || current_user.product_account
+      params[:followers] ? receipents = current_user.followers : recepents = Person.where.not(id: current_user.id)
+        receipents = current_user.followers
 
-#   private
+      end
+    else
+      return render_401 _('You are not allowed to send notifications.')
+    end
+  end
 
-#   def certificate_params
-#     params.require(:notification).permit(:body, :flag)
-#   end
-# end
+  private
+
+  def certificate_params
+    params.require(:notification).permit(:body, :followers)
+  end
+end
