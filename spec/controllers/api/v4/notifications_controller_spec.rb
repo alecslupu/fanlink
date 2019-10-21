b7d5393f0961a2a8247e225aade24dc89ea52a2f
Cristian Somesan <crs@flink.to>
2019-10-21 13:47:42 +0300

    Add tests(work in progress)

---
+++ spec/controllers/api/v4/notifications_controller_spec.rb
@@ -0,0 +1,12 @@
+# require 'rails_helper'
+
+# RSpec.describe Api::V4::NotificationsController, type: :controller do
+#   describe '#create' do
+#     it 'creates a new notification for a user ' do
+#       person = create(:person, pin_messages_from: true)
+#       ActsAsTenant.with_tenant(person.product) do
+#         login_as(person)
+#       end
+#     end
+#   end
+# end
