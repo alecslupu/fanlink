930408176f3846bf522f2ae537b590305c45bdc5
Cristian Somesan <crs@flink.to>
2019-10-21 12:50:25 +0300

    More refactoring

--- app/jobs/simple_notification_push_job.rb
+++ app/jobs/simple_notification_push_job.rb
@@ -4,7 +4,15 @@ class SimpleNotificationPushJob < Struct.new(:notification_id)
   def perform
     notification = Notification.find(notification_id)
     ActsAsTenant.with_tenant(notification.person.product) do
-      simple_notification_push(notification)
+      current_user = Person.find(notification.person_id)
+
+      if notification.for_followers
+        receipents = current_user.followers
+      else
+        receipents = Person.where.not(id: current_user.id)
+      end
+
+      simple_notification_push(notification,current_user, receipents)
     end
   end
 
