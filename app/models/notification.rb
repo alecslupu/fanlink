baf8ae5674461e0e36ad934312cdef444534f9b1
Cristian Somesan <crs@flink.to>
2019-10-21 13:13:50 +0300

    Add product id to notifications

--- app/models/notification.rb
+++ app/models/notification.rb
@@ -5,10 +5,13 @@
 #  id                     :bigint(8)        not null, primary key
 #  body                   :text             not null
 #  person_id              :integer          foreign key
+#  product_id             :integer          not null
 #
 
 class Notification < ApplicationRecord
   include Notification::RealTime
+
+  acts_as_tenant(:product)
 
   belongs_to :person
   validates :body, presence: true
