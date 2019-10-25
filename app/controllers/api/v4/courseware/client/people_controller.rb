6d4f78da878dd87601c43d524813fbc7a07f47d0
Cristian Somesan <crs@flink.to>
2019-10-25 18:54:55 +0300

    Add permission + #index method on courseware client people controller

--- app/controllers/api/v4/courseware/client/people_controller.rb
+++ app/controllers/api/v4/courseware/client/people_controller.rb
@@ -1,4 +1,15 @@
 class Api::V4::Courseware::Client::PeopleController < ApiController
   def index
+    if current_user.client?
+      @assignees = current_user.assignees
+      # return the @assignees
+    else
+      render_401 _("You must have 'client' role to access this feature.")
+    end
+  end
+
+  private
+  def person_params
+    params.require(:person).permit(:client_id)
   end
 end
