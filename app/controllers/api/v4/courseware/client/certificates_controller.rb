3a968aaa31b44bc0821582660b5d1ce433fb584b
Cristian Somesan <crs@flink.to>
2019-10-29 19:05:31 +0200

    Add serializer for certificate#download + mock the response

--- app/controllers/api/v4/courseware/client/certificates_controller.rb
+++ app/controllers/api/v4/courseware/client/certificates_controller.rb
@@ -1,6 +1,7 @@
 class Api::V4::Courseware::Client::CertificatesController < ApiController
 
   # o sa dai certificarile persoane cu id-ul params[:person_id]
+  # PersonCertificate.last.issued_certificate_image.url poza
 
   def index
     id = Certificate.last.nil? ? 1 : Certificate.last.id
@@ -54,5 +55,9 @@ class Api::V4::Courseware::Client::CertificatesController < ApiController
         product_id: current_user.product.id
       )
       return_the @certificates, handler: :jb
+  end
+
+  def download
+    @url = "https://s3.us-east-1.amazonaws.com/fanlink-staging/caned/certificates/template_images/000/000/086/original/76ba71377a4b46a2d2c72921c0e62bc3b3b9d6cf.jpg?1571836879"
   end
 end
