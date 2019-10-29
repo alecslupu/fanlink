da1e58912f92892e4529202e01cc7318d6a7799e
Cristian Somesan <crs@flink.to>
2019-10-29 19:10:05 +0200

    FLAPI-1218 Add to mock response for send_email

--- app/controllers/api/v4/courseware/client/certificates_controller.rb
+++ app/controllers/api/v4/courseware/client/certificates_controller.rb
@@ -60,4 +60,8 @@ class Api::V4::Courseware::Client::CertificatesController < ApiController
   def download
     @url = "https://s3.us-east-1.amazonaws.com/fanlink-staging/caned/certificates/template_images/000/000/086/original/76ba71377a4b46a2d2c72921c0e62bc3b3b9d6cf.jpg?1571836879"
   end
+
+  def send_email
+    render json: { message: _("Email sent") }
+  end
 end
