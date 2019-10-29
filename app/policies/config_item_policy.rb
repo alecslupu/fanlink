4028abfae4c9d642a687d52b331a59eb85be6e6b
Alexandru-Emil Lupu <contact@alecslupu.ro>
2019-10-29 18:37:58 +0200

    Fixing policies for ConfigItem - FLAPI-1224

--- app/policies/config_item_policy.rb
+++ app/policies/config_item_policy.rb
@@ -1,4 +1,11 @@
 class ConfigItemPolicy < ApplicationPolicy
+
+  class Scope < ApplicationPolicy::Scope
+    def resolve
+      scope.for_product(ActsAsTenant.current_tenant)
+    end
+  end
+
   protected
   def module_name
     "root"
