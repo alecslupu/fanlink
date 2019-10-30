789faf9e50937fb1f6fa9e01f0e000a07dcf5bf2
Alexandru-Emil Lupu <contact@alecslupu.ro>
2019-10-30 15:55:56 +0200

    FIXING FLAPI-1236

--- app/policies/config_item_policy.rb
+++ app/policies/config_item_policy.rb
@@ -1,5 +1,9 @@
 class ConfigItemPolicy < ApplicationPolicy
 
+  def nested_set?
+    edit?
+  end
+  
   class Scope < ApplicationPolicy::Scope
     def resolve
       scope.for_product(ActsAsTenant.current_tenant)
