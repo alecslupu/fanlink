07f4a0afaf581efbe012482498f94509788e21e6
Cristian Somesan <crs@flink.to>
2019-10-31 14:38:14 +0200

    FLAPI-1218 Add to check if searched person is a assignee of current user

--- app/controllers/api/v4/courseware/client/base_controller.rb
+++ app/controllers/api/v4/courseware/client/base_controller.rb
@@ -1,9 +1,17 @@
 class Api::V4::Courseware::Client::BaseController < ApiController
   before_action :authorize_person
+  before_action :check_if_clients_assignee
 
   private
 
   def authorize_person
     return render_401 _("You must have the 'client' role to access this feature.") unless current_user.client?
+  end
+
+  def check_if_clients_assignee
+    binding.pry
+    if params[:person_id].present? && Person.find(params[:person_id]).assigners.exclude?(current_user)
+      return render_401 _("You can only see your assignee's info.")
+    end
   end
 end
