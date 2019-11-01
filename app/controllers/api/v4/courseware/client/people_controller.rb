450da1aef4ac2ef27cc607664034728c70220558
Cristian Somesan <crs@flink.to>
2019-11-01 15:12:41 +0000

    FLAPI-1220 Add to  order response for courseware client #index action

--- app/controllers/api/v4/courseware/client/people_controller.rb
+++ app/controllers/api/v4/courseware/client/people_controller.rb
@@ -3,22 +3,13 @@ class Api::V4::Courseware::Client::PeopleController < ApiController
     if current_user.client?
       id = Person.last.nil? ? 1 : Person.last.id
       if params[:username_filter].present?
-        @assignees = paginate (Person.all.username_filter(params[:username_filter], current_user))
+        @assignees = paginate (Person.all.username_filter_courseware(params[:username_filter]).order(:username))
       else
-        @assignees = paginate (Person.all)
+        @assignees = paginate (Person.all.order(:username))
       end
       return_the @assignees, handler: :jb
     else
       render_401 _("You must have 'client' role to access this feature.")
     end
   end
-
-  # def index
-  #   if current_user.client?
-  #     @assignees = current_user.assignees
-  #     return_the @assignees, handler: :jb
-  #   else
-  #     render_401 _("You must have 'client' role to access this feature.")
-  #   end
-  # end
 end
