5a76af562aa14f40c19847688c71553efa901441
Cristian Somesan <crs@flink.to>
2019-11-07 10:24:21 +0200

    Rubocop changes

--- app/controllers/api/v4/courseware/client/certcourses_controller.rb
+++ app/controllers/api/v4/courseware/client/certcourses_controller.rb
@@ -8,7 +8,7 @@ class Api::V4::Courseware::Client::CertcoursesController < Api::V4::Courseware::
       @certcourses = @certificate.certcourses
       return_the @certcourses, handler: :jb
     else
-      render_422 _('This user does not have the requested certificate.')
+      render_422 _("This user does not have the requested certificate.")
     end
   end
 
@@ -37,7 +37,7 @@ class Api::V4::Courseware::Client::CertcoursesController < Api::V4::Courseware::
         #   is_correct = true
         #   answer_text = person_response.fill_in_response.present?
         else
-          answer_text =  Answer.find(failed_attempts.last.answer_id).description
+          answer_text = Answer.find(failed_attempts.last.answer_id).description
           is_correct = false
         end
 
@@ -56,39 +56,9 @@ class Api::V4::Courseware::Client::CertcoursesController < Api::V4::Courseware::
         @quizzes << quiz
       end
 
-    return_the @quizzes, handler: :jb
+      return_the @quizzes, handler: :jb
     else
       render_404(_("This certificates has no quiz page."))
     end
   end
 end
-    # <PersonQuiz id: 1, person_id: 36505, quiz_page_id: 2, answer_id: 6, fill_in_response: nil, created_at: "2019-03-16 02:26:21",
-    # updated_at: "2019-03-16 02:26:21">,
-
-    #<PersonQuiz id: 2, person_id: 36505, quiz_page_id: 3, answer_id: 7, fill_in_response: nil,
-    # created_at: "2019-03-16 02:26:35", updated_at: "2019-03-16 02:26:35">
-
-
-    # <Answer id: 6, quiz_page_id: 2, description: "Prepare your interview outfit the morning of your ...", is_correct: true,
-    # created_at: "2019-03-15 21:36:25", updated_at: "2019-03-15 21:36:25", product_id: 17>
-
-    # <QuizPage id: 2, certcourse_page_id: 30, is_optional: false, quiz_text: " To prepare for an interview, which of the follow ...",
-    # wrong_answer_page_id: 27, created_at: "2019-03-15 21:35:09", updated_at: "2019-03-16 04:22:46", product_id: 17, is_survey: false>
-
-    # <Certcourse id: 1, long_name: "cacacaa", short_name: "cac", description: "Test no pages 8", color_hex: "#000000", status: "entry",
-    # duration: 5568798, is_completed: false, copyright_text: "text", created_at: "2019-03-11 18:34:20", updated_at: "2019-10-08 11:50:16",
-    # product_id: 2, certcourse_pages_count: 5>
-
-    # @quizzes = [quiz1, quiz2, quiz3, quiz4, quiz5]
-
-    # quiz1 = {
-    #   id: 1, +
-    #   is_optional: false, +
-    #   no_of_failed_attempts: rand(20),  +
-    #   quiz_text: Faker::Lorem.sentence, +
-    #   answer_text: Faker::Lorem.sentence,
-    #   certcourse_pages_count: 25,     +
-    #   page_order: 12,   +
-    #   is_correct: true,  +
-    #   is_survey          +
-    # }
