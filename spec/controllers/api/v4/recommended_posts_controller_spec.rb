8b4bc8ea90799f6141f833d97c657b15b74f00b9
Cristian Somesan <crs@flink.to>
2019-11-11 19:02:41 +0200

    FLAPI-1257 Add the possibility to not paginate the response for recommended posts#index + tests

--- spec/controllers/api/v4/recommended_posts_controller_spec.rb
+++ spec/controllers/api/v4/recommended_posts_controller_spec.rb
@@ -3,21 +3,20 @@ require "spec_helper"
 RSpec.describe Api::V4::RecommendedPostsController, type: :controller do
   # TODO: auto-generated
   describe "GET index" do
-    it "should get all recommended posts" do
+    it "should get all recommended posts when not providing page param" do
       person = create(:person)
       ActsAsTenant.with_tenant(person.product) do
-        create_list(:recommended_post, 8, status: :published)
+        create_list(:recommended_post, 26, status: :published) #default per_page number is 25
         draft = create(:post)
         published_post = create(:published_post)
         login_as(person)
         get :index
         expect(response).to be_successful
         posts = json["recommended_posts"].map { |p| p["id"].to_i }
-        expect(posts.size).to eq(8)
-        expect(posts).not_to include(published_post.id)
-        expect(posts).not_to include(draft.id)
+        expect(posts.size).to eq(26)
       end
     end
+
     it "should get page 1 of recommended posts" do
       person = create(:person)
       ActsAsTenant.with_tenant(person.product) do
@@ -30,6 +29,7 @@ RSpec.describe Api::V4::RecommendedPostsController, type: :controller do
         expect(posts.sort).to eq([post_list[0].id, post_list[1].id].sort)
       end
     end
+
     it "should get page 2 of recommended posts" do
       person = create(:person)
       ActsAsTenant.with_tenant(person.product) do
