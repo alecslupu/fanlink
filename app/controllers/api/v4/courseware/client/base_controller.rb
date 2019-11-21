8b44be9d7346f9ee373421b8bd64b4c3c586d0fb
Cristian Somesan <crs@flink.to>
2019-11-21 15:44:06 +0200

    Style changes

--- app/controllers/api/v4/courseware/client/base_controller.rb
+++ app/controllers/api/v4/courseware/client/base_controller.rb
@@ -5,18 +5,18 @@ class Api::V4::Courseware::Client::BaseController < ApiController
   before_action :check_if_clients_assignee
 
   protected
-  def assignee_ids
-    current_user.hired_people.pluck(:person_id)
-  end
+    def assignee_ids
+      current_user.hired_people.pluck(:person_id)
+    end
 
   private
 
-  def authorize_person
-    raise AccessDeniedException.new _("You must have the 'client' role to access this feature.") unless current_user.client?
-  end
+    def authorize_person
+      raise AccessDeniedException.new _("You must have the 'client' role to access this feature.") unless current_user.client?
+    end
 
-  def check_if_clients_assignee
-    raise AccessDeniedException.new _("You need to supply a person_id.") unless params[:person_id].present?
-    raise AccessDeniedException.new _("You can only see your assignee's info.") unless assignee_ids.include?(params[:person_id].to_i)
-  end
+    def check_if_clients_assignee
+      raise AccessDeniedException.new _("You need to supply a person_id.") unless params[:person_id].present?
+      raise AccessDeniedException.new _("You can only see your assignee's info.") unless assignee_ids.include?(params[:person_id].to_i)
+    end
 end
--- app/controllers/api/v4/courseware/client/people_controller.rb
+++ app/controllers/api/v4/courseware/client/people_controller.rb
@@ -2,6 +2,7 @@ class Api::V4::Courseware::Client::PeopleController < Api::V4::Courseware::Clien
   # frozen_string_literal: true
 
   skip_before_action :check_if_clients_assignee
+
   def index
     @assignees = Person.where(id: assignee_ids)
     @assignees = @assignees.username_filter(params[:username_filter], current_user) if params[:username_filter].present?
