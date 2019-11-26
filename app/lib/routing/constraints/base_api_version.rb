16c3b3e64564a1abe9da1dd92fc100e70cda17f9
Alexandru-Emil Lupu <contact@alecslupu.ro>
2019-11-26 18:06:27 +0200

    Version fix

--- app/lib/routing/constraints/base_api_version.rb
+++ app/lib/routing/constraints/base_api_version.rb
@@ -1,14 +1,14 @@
 module Routing
   module Constraints
     class BaseApiVersion
-      protected
-      def api_version(version)
-        "application/vnd.api."+version+"+json"
-      end
-
 
       def self.matches?(request, version)
         request.headers['Accept'] && request.headers['Accept'] == api_version(version)
+      end
+
+      protected
+      def self.api_version(version)
+        "application/vnd.api."+version+"+json"
       end
     end
   end
