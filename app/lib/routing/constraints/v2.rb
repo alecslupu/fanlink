cb912ec6fd05490bb82990c26bc40aed3f98ad72
Alexandru-Emil Lupu <contact@alecslupu.ro>
2019-11-26 19:38:47 +0200

    Fixing Rails admin

--- app/lib/routing/constraints/v2.rb
+++ app/lib/routing/constraints/v2.rb
@@ -2,7 +2,7 @@ module Routing
   module Constraints
     class V2 < BaseApiVersion
       def self.matches?(request)
-        super request, "v3"
+        super request, "v2"
       end
     end
   end
