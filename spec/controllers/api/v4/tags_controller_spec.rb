17d21341357d8a2532089ff8f3fb07a69bf43324
Cristian Somesan <crs@flink.to>
2019-12-13 13:39:44 +0200

    FLAPI-1166 Refactor partial for posts#index and tags#index for posts + tests

--- spec/controllers/api/v4/tags_controller_spec.rb
+++ spec/controllers/api/v4/tags_controller_spec.rb
@@ -9,14 +9,23 @@ RSpec.describe Api::V4::TagsController, type: :controller do
         login_as(person)
         person2 = create(:person)
         tag = create(:tag)
-        post = create(:published_post,  person: person2)
+        post = create(:published_post, person: person2)
+        1.upto 4 do
+          create(:post_reaction, post: post)
+        end
         post.tags << tag
+
+        create(:post_reaction, person: person, post: post)
 
         person.follow(person2)
         person2.follow(person)
 
         get :index, params: { tag_name: tag.name }
         expect(response).to be_successful
+        post = json["posts"].first
+
+        expect(post["post_reaction"]).not_to eq(nil)
+        expect(post["post_reaction_counts"]).not_to eq(nil)
       end
     end
   end
