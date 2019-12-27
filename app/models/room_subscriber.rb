a30e248ddd245d16e4dd9b4311c7267f5ff9b7fa
Cristian Somesan <crs@flink.to>
2019-12-27 19:37:19 +0200

    FLAPi-1320 Add validation to verify that the room is public before adding owners

--- app/models/room_subscriber.rb
+++ app/models/room_subscriber.rb
@@ -3,5 +3,13 @@ class RoomSubscriber < ApplicationRecord
   belongs_to :person
   belongs_to :last_message, class_name: "Message", optional: true
 
+  validate :check_private
+
   scope :for_product, ->(product) { joins(:person).where( people: { product_id: product.id }) }
+
+  private
+
+  def check_private
+    errors.add(:room_id, :not_public, message: _("must be public to have owners.")) if room.private?
+  end
 end
