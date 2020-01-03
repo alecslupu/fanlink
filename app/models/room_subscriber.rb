acd8a83820c49af7bd1c85851aa3fec13db3855c
Cristian Somesan <crs@flink.to>
2020-01-03 13:32:25 +0200

    FLAPI-1320 Add before save hook to change last notification time

--- app/models/room_subscriber.rb
+++ app/models/room_subscriber.rb
@@ -5,11 +5,17 @@ class RoomSubscriber < ApplicationRecord
 
   validate :check_private
 
+  before_create :set_last_notification_time
+
   scope :for_product, ->(product) { joins(:person).where( people: { product_id: product.id }) }
 
   private
 
     def check_private
       errors.add(:room_id, :not_public, message: _("must be public to have owners.")) if room.private?
+    end
+
+    def set_last_notification_time
+      self.last_notification_time = DateTime.now - 2.minutes
     end
 end
