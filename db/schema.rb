b9b1c0d02ff81d1905ee9b4f44f113b19482b73b
Cristian Somesan <crs@flink.to>
2020-01-03 14:16:56 +0200

    FLAPI-1320 Add index on room subscriber to permit only unique room_id-person_id pairs

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20191226081348) do
+ActiveRecord::Schema.define(version: 20200103121328) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -1087,6 +1087,7 @@ ActiveRecord::Schema.define(version: 20191226081348) do
     t.datetime "updated_at", null: false
     t.index ["last_message_id"], name: "index_room_subscribers_on_last_message_id"
     t.index ["person_id"], name: "index_room_subscribers_on_person_id"
+    t.index ["room_id", "person_id"], name: "unq_room_person", unique: true
     t.index ["room_id"], name: "index_room_subscribers_on_room_id"
   end
 
