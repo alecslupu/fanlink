ef2d08cfeb88179e9dcb799651605e2c8b2e1d5c
Cristian Somesan <crs@flink.to>
2020-01-08 20:52:14 +0200

    FLAPI-1286 Add subscribe and unsubscribe to topic delayed jobs

---
+++ app/jobs/subscribe_to_topic_job.rb
@@ -0,0 +1,17 @@
+class SubscribeToTopicJob < Struct.new(:notification_device_id)
+  include Push
+
+  def perform
+    notification_device =  NotificationDeviceId.find(notification_device_id)
+    ActsAsTenant.with_tenant(notification_device.person.product) do
+      # TODO add  topic option
+      subscribe_device_to_topic(notification_device)
+    end
+  end
+
+  def error(job, exception)
+    if exception.is_a?(ActiveRecord::RecordNotFound)
+      job.destroy
+    end
+  end
+end
