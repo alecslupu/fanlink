0f66d3966999dd63ac38c732a1f2ee2f4de43ed7
Cristian Somesan <crs@flink.to>
2020-01-10 18:38:29 +0200

    FLAPI-1286 Add validations for the new field + setting; add the new field in rails module

--- app/models/marketing_notification.rb
+++ app/models/marketing_notification.rb
@@ -2,11 +2,14 @@
 #
 # Table name: marketing_notifications
 #
-#  id         :bigint(8)        not null, primary key
-#  title      :string           not null
-#  body       :text             not null
-#  person_id  :integer          not null, foreign key
-#  product_id :integer          not null, foreign key
+#  id                :bigint(8)        not null, primary key
+#  title             :string           not null
+#  body              :text             not null
+#  person_id         :integer          not null, foreign key
+#  ttl_hours         :integer          not null, default: 670
+#  deep_link_action  :integer          not null, default: 0
+#  deep_link_value   :string
+#  product_id        :integer          not null, foreign key
 #
 
 class MarketingNotification < ApplicationRecord
@@ -14,8 +17,22 @@ class MarketingNotification < ApplicationRecord
 
   acts_as_tenant(:product)
 
+  enum deep_link_action: {
+    send_to_all: 0,
+    comments_screen: 1,
+    profile_screen: 2,
+    posts_screen: 3,
+    feed_screen: 4,
+    profile_screen: 5,
+    chat_room: 6,
+    comments_screen: 7,
+    certificate_screen: 8
+  }
+
   validates :body, presence: true
   validates :title, presence: true
+  validates :ttl_hours, presence: true
+  validates :deep_link_action, presence: true
 
   after_create :notify
 
