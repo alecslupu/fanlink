18948c9b3666c356fdfa88cd0acaca987c009b45
Cristian Somesan <crs@flink.to>
2020-01-14 10:08:55 +0200

    FLAPI-1312 Add to include polls in recommended posts

--- spec/controllers/api/v4/recommended_posts_controller_spec.rb
+++ spec/controllers/api/v4/recommended_posts_controller_spec.rb
@@ -3,7 +3,7 @@ require "spec_helper"
 RSpec.describe Api::V4::RecommendedPostsController, type: :controller do
   # TODO: auto-generated
   describe "GET index" do
-    it "should get all recommended posts when not providing page param" do
+    it "gets all recommended posts when not providing page param" do
       person = create(:person)
       ActsAsTenant.with_tenant(person.product) do
         create_list(:recommended_post, 26, status: :published) #default per_page number is 25
@@ -17,7 +17,7 @@ RSpec.describe Api::V4::RecommendedPostsController, type: :controller do
       end
     end
 
-    it "should get page 1 of recommended posts" do
+    it "gets page 1 of recommended posts" do
       person = create(:person)
       ActsAsTenant.with_tenant(person.product) do
         post_list = create_list(:recommended_post, 8, status: :published).reverse
@@ -30,12 +30,25 @@ RSpec.describe Api::V4::RecommendedPostsController, type: :controller do
       end
     end
 
-    it "should get page 2 of recommended posts" do
+    it "gets page 2 of recommended posts" do
       person = create(:person)
       ActsAsTenant.with_tenant(person.product) do
         post_list = create_list(:recommended_post, 8, status: :published).reverse
         login_as(person)
         get :index, params: {page: 2, per_page: 2}
+        expect(response).to be_successful
+        posts = json["recommended_posts"].map { |p| p["id"].to_i }
+        expect(posts.size).to eq(2)
+        expect(posts.sort).to eq([post_list[2].id, post_list[3].id].sort)
+      end
+    end
+
+    it "includes the polls" do
+      person = create(:person)
+      ActsAsTenant.with_tenant(person.product) do
+        post_list = create_list(:recommended_post, 2, status: :published).reverse
+        login_as(person)
+        get :index
         expect(response).to be_successful
         posts = json["recommended_posts"].map { |p| p["id"].to_i }
         expect(posts.size).to eq(2)
