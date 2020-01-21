48e76ac8d66777f10d58995b698adb4d405f90f1
Cristian Somesan <crs@flink.to>
2020-01-21 18:11:14 +0200

    FLAPi-1374 Add tests WIP

--- spec/controllers/api/v4/interests_controller_spec.rb
+++ spec/controllers/api/v4/interests_controller_spec.rb
@@ -15,4 +15,21 @@ RSpec.describe Api::V4::InterestsController, type: :controller do
   describe "PUT update" do
     pending
   end
+
+  describe "GET match" do
+    it "gets all the people that have at least one of the interests, except current user" do
+
+    end
+  end
 end
+
+    it "should not change the current users password if it matches the current one" do
+      password = "password"
+      person = create(:person, password: password)
+      ActsAsTenant.with_tenant(person.product) do
+        login_as(person)
+        patch :change_password,  params: { id: person.id, person: { current_password: password, new_password: password } }
+        expect(response).to be_unprocessable
+        expect(json["errors"]).to include("New password can't be identical to your current one")
+      end
+    end
