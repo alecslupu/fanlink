e8e7e71024c06de7a3bfed156a53323d5963c514
Cristian Somesan <crs@flink.to>
2020-01-22 14:42:50 +0200

    FLAPI-1374 More refactoring + add tests

--- app/models/person.rb
+++ app/models/person.rb
@@ -484,6 +484,7 @@ class Person < ApplicationRecord
     self.select("people.*, array_agg(DISTINCT person_interests.interest_id) as matched_ids")
       .joins(:person_interests).where("person_interests.interest_id in (?)", interest_ids)
       .where("person_interests.person_id != (?)", person_id)
+      .where("people.product_account = false")
       .group("people.id")
       .order("count(person_interests.*) DESC")
   end
--- spec/controllers/api/v4/interests_controller_spec.rb
+++ spec/controllers/api/v4/interests_controller_spec.rb
@@ -17,7 +17,7 @@ RSpec.describe Api::V4::InterestsController, type: :controller do
   end
 
   describe "GET match" do
-    it "gets all the people that have at least one of the given interests, except current user" do
+    it "gets all the people that have at least one of the given interests, except current user, in no of interests desc order" do
       current_user = create(:person)
       ActsAsTenant.with_tenant(current_user.product) do
         login_as(current_user)
@@ -27,24 +27,38 @@ RSpec.describe Api::V4::InterestsController, type: :controller do
         create(:person_interest, person: current_user, interest: interests.first)
         create(:person_interest, person: current_user, interest: interests.second)
         create(:person_interest, person: people.first, interest: interests.first)
+        create(:person_interest, person: people.second, interest: interests.second)
         create(:person_interest, person: people.second, interest: interests.third)
 
-        # interest2 = create(:interest)
+        get :match, params: { interest_ids: "#{interests.first.id},#{interests.second.id}, #{interests.third.id}" }
+
+        expect(response).to be_successful
+        expect(json["matches"].map { |a_match| a_match["person"]["id"].to_i }).not_to include(current_user)
+        expect(json["matches"].first["person"]["id"].to_i).to eq(people.second.id)
+        expect(json["matches"].first["matched_interest_ids"].sort).to eq(interests.last(2).pluck(:id).sort)
+        expect(json["matches"].second["person"]["id"].to_i).to eq(people.first.id)
+        expect(json["matches"].second["matched_interest_ids"].sort).to eq([interests.first.id])
+      end
+    end
+
+    it "does not return people with product accounts" do
+      current_user = create(:person)
+      ActsAsTenant.with_tenant(current_user.product) do
+        login_as(current_user)
+
+        product_account_person = create(:person, product_account: true)
+        person2 = create(:person, product_account: false)
+        interests = create_list(:interest, 3)
+        create(:person_interest, person: product_account_person, interest: interests.first)
+        create(:person_interest, person: person2, interest: interests.third)
 
         get :match, params: { interest_ids: "#{interests.first.id},#{interests.second.id}, #{interests.third.id}" }
-        binding.pry
+
+        expect(response).to be_successful
+        person_ids = json["matches"].map { |a_match| a_match["person"]["id"].to_i }
+        expect(person_ids).not_to include(product_account_person.id)
+        expect(person_ids).to include(person2.id)
       end
     end
   end
 end
-
-    # it "should not change the current users password if it matches the current one" do
-    #   password = "password"
-    #   person = create(:person, password: password)
-    #   ActsAsTenant.with_tenant(person.product) do
-    #     login_as(person)
-    #     patch :change_password,  params: { id: person.id, person: { current_password: password, new_password: password } }
-    #     expect(response).to be_unprocessable
-    #     expect(json["errors"]).to include("New password can't be identical to your current one")
-    #   end
-    # end
