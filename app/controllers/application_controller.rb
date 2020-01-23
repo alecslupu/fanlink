fd5c4220c68f1ab7484af65ce943e7bf0743ef1b
Cristian Somesan <crs@flink.to>
2020-01-23 15:00:45 +0200

    FLAPI-1286 Add to get current user from person model

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,12 @@ class ApplicationController < ActionController::Base
   # protect_from_forgery with: :exception
   before_action :require_login
 
+  before_action :set_current_user
+
+  def set_current_user
+    Person.current_user = current_user
+  end
+
   def status
     head :ok
   end
