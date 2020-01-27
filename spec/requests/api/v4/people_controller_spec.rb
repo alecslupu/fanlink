346e77459b87c643f860b9376fad747c0b921984
Alexandru-Emil Lupu <contact@alecslupu.ro>
2020-01-27 18:32:39 +0200

    Fixing apidoc

--- spec/requests/api/v4/people_controller_spec.rb
+++ spec/requests/api/v4/people_controller_spec.rb
@@ -89,7 +89,7 @@ RSpec.describe "Api::V4::PeopleController", type: :request, swagger_doc: "v4/swa
       parameter name: :"person[password]", in: :formData, type: :string, required: false
       parameter name: :"person[picture]", in: :formData,  type: :file
       parameter name: :facebook_auth_token, in: :formData, type: :string, required: false
-      parameter name: :referer, in: :formData,  type: :string, required: false
+      parameter name: :referrer, in: :formData,  type: :string, required: false
 
       let(:product) { }
       let("person[picture]") {  }
