4dd345687fe5ac1bd5cc525440874e30187c9fa7
Cristian Somesan <crs@flink.to>
2020-01-29 12:00:11 +0200

    FLAPI-1382 Remove pagination

--- app/controllers/api/v4/referral/referral_controller.rb
+++ app/controllers/api/v4/referral/referral_controller.rb
@@ -1,12 +1,12 @@
 class Api::V4::Referral::ReferralController < ApiController
 
   def index
-    @people = paginate(current_user.referred_people)
+    @people = current_user.referred_people
     render :index, handler: :jb
   end
 
   def purchased
-    @people = paginate(current_user.referred_people.joins(:certificates).where(certificates: {is_free: false}))
+    @people = current_user.referred_people.joins(:certificates).where(certificates: {is_free: false})
     render :index, handler: :jb
   end
 end
