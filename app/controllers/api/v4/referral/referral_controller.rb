467fc25a3ea41265f765b9acfa32b44c70f0ea47
Cristian Somesan <crs@flink.to>
2020-01-29 12:11:56 +0200

    FLAPI-1382 Add mock response for referral

--- app/controllers/api/v4/referral/referral_controller.rb
+++ app/controllers/api/v4/referral/referral_controller.rb
@@ -1,12 +1,14 @@
 class Api::V4::Referral::ReferralController < ApiController
 
   def index
-    @people = current_user.referred_people
+    # @people = current_user.referred_people
+    @people = Person.Person.order("RANDOM()").limit(10)
     render :index, handler: :jb
   end
 
   def purchased
-    @people = current_user.referred_people.joins(:certificates).where(certificates: {is_free: false})
+    # @people = current_user.referred_people.joins(:certificates).where(certificates: {is_free: false})
+    @people = Person.order("RANDOM()").limit(10)
     render :index, handler: :jb
   end
 end
