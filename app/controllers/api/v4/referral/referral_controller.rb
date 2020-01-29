299f117a8b6ef387d7f9d2ae38322f4108def7ef
Cristian Somesan <crs@flink.to>
2020-01-29 12:28:38 +0200

    FLAPI-1382 Add mock response

--- app/controllers/api/v4/referral/referral_controller.rb
+++ app/controllers/api/v4/referral/referral_controller.rb
@@ -2,13 +2,13 @@ class Api::V4::Referral::ReferralController < ApiController
 
   def index
     # @people = current_user.referred_people
-    @people = Person.Person.order("RANDOM()").limit(10)
+    @people = Person.order("RANDOM()").limit(rand(10..15))
     render :index, handler: :jb
   end
 
   def purchased
     # @people = current_user.referred_people.joins(:certificates).where(certificates: {is_free: false})
-    @people = Person.order("RANDOM()").limit(10)
+    @people = Person.order("RANDOM()").limit(rand(5..10))
     render :index, handler: :jb
   end
 end
