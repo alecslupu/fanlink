70e72299472adcce2b5b2bd8b209154eeb9c4534
Cristian Somesan <crs@flink.to>
2020-01-31 15:53:21 +0200

    FLAPI-1286 Small tweak

--- app/jobs/inactive48h_notification_job.rb
+++ app/jobs/inactive48h_notification_job.rb
@@ -4,11 +4,11 @@ class Inactive48hNotificationJob
   def perform
     notification = AutomatedNotification.where(criteria: :inactive_48h, enabled: true).last
     ActsAsTenant.with_tenant(notification.product) do
-      NotificationDeviceId.joins(:person).where(device_type: :ios).where("people.last_activity_at > ? AND people.last_activity_at < ?",Time.zone.now - 49.hour, Time.zone.now - 48.hour).select(:id).find_in_batches(batch_size: BATCH_SIZE) do |device_ids|
+      NotificationDeviceId.joins(:person).where(device_type: :ios).where("people.last_activity_at > ? AND people.last_activity_at < ?",Time.zone.now - 51.hour, Time.zone.now - 48.hour).select(:id).find_in_batches(batch_size: BATCH_SIZE) do |device_ids|
         Delayed::Job.enqueue(AutomatedNotificationIosPushJob.new(device_ids.pluck(:id), notification.title, notification.body, notification.ttl_hours))
       end
 
-      NotificationDeviceId.joins(:person).where(device_type: :android).where("people.last_activity_at > ? AND people.last_activity_at < ?",Time.zone.now - 49.hour, Time.zone.now - 48.hour).select(:id).find_in_batches(batch_size: BATCH_SIZE) do |device_ids|
+      NotificationDeviceId.joins(:person).where(device_type: :android).where("people.last_activity_at > ? AND people.last_activity_at < ?",Time.zone.now - 51.hour, Time.zone.now - 48.hour).select(:id).find_in_batches(batch_size: BATCH_SIZE) do |device_ids|
         Delayed::Job.enqueue(AutomatedNotificationAndroidPushJob.new(device_ids.pluck(:id), notification.title, notification.body, notification.ttl_hours))
       end
     end
