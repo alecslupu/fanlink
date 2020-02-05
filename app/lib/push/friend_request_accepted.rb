0c6bc5d5726411771266ec8bc5de0086960a5635
Cristian Somesan <crs@flink.to>
2020-02-05 17:31:39 +0200

    FLAPI-1286 Refactor friend accepted notification

--- app/lib/push/friend_request_accepted.rb
+++ app/lib/push/friend_request_accepted.rb
@@ -3,7 +3,6 @@ module Push
 
     def push(relationship)
       @target_person = relationship.requested_to
-      from_person = relationship.requested_by
 
       if relationship.friended?
         android_token_notification_push(
@@ -12,7 +11,7 @@ module Push
           title: "Friend request accepted by #{@target_person.username}",
           message_short: "Friend request accepted by #{@target_person.username}",
           message_placeholder: @target_person.username,
-          deep_link: "#{from_person.product.internal_name}://users/#{@target_person.id}/profile"
+          deep_link: "#{@target_person.product.internal_name}://users/#{@target_person.id}/profile"
         )
 
         ios_token_notification_push(
@@ -21,7 +20,7 @@ module Push
           nil,
           2419200,
           context: "friend_accepted",
-          deep_link: "#{from_person.product.internal_name}://users/#{@target_person.id}/profile"
+          deep_link: "#{@target_person.product.internal_name}://users/#{@target_person.id}/profile"
         )
       end
     end
