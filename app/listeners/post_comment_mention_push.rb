cf1227ac0edb9298f0c13d8f5565ff27c370aab0
Cristian Somesan <crs@flink.to>
2020-02-06 12:04:52 +0200

    FLAPI-1286 Try to solve issue with message mention notification

--- app/listeners/post_comment_mention_push.rb
+++ app/listeners/post_comment_mention_push.rb
@@ -2,7 +2,7 @@ class PostCommentMentionPush < BaseMention
   def self.post_comment_created(post_comment_id, product_id)
     post_comment = PostComment.find(post_comment_id)
     if post_comment.body
-      mentions = self.parse_content(post_comment.body, product_id)
+      mentions = self.parse_body_content(post_comment.body, product_id)
       if mentions
         mentions.each do |mentioned|
           blocks_with = post_comment.person.blocks_with.map { |b| b.id }
