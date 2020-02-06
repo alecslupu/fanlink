1588a2d9cb8657be59706e3fb87b38d10a9789e5
Cristian Somesan <crs@flink.to>
2020-02-06 15:51:08 +0200

    FLAPI-1286 Resolve issue with friend request accepted + add to send notification in batches if number of tokens exceeds 500

--- app/lib/push/friend_request_accepted.rb
+++ app/lib/push/friend_request_accepted.rb
@@ -3,24 +3,25 @@ module Push
 
     def push(relationship)
       @target_person = relationship.requested_by
+      requested_to = relationship.requested_to
 
       if relationship.friended?
         android_token_notification_push(
           2419200,
           context: "friend_accepted",
-          title: "Friend request accepted by #{@target_person.username}",
-          message_short: "Friend request accepted by #{@target_person.username}",
-          message_placeholder: @target_person.username,
-          deep_link: "#{@target_person.product.internal_name}://users/#{@target_person.id}/profile"
+          title: "Friend request accepted by #{requested_to.username}",
+          message_short: "Friend request accepted by #{requested_to.username}",
+          message_placeholder: requested_to.username,
+          deep_link: "#{requested_to.product.internal_name}://users/#{requested_to.id}/profile"
         )
 
         ios_token_notification_push(
           "Friend request accepted",
-          "Friend request accepted by #{@target_person.username}",
+          "Friend request accepted by #{requested_to.username}",
           nil,
           2419200,
           context: "friend_accepted",
-          deep_link: "#{@target_person.product.internal_name}://users/#{@target_person.id}/profile"
+          deep_link: "#{requested_to.product.internal_name}://users/#{requested_to.id}/profile"
         )
       end
     end
