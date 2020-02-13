151ac65f06b0892bd0c8befb1fcbc15def71ea9d
Cristian Somesan <crs@flink.to>
2020-02-13 19:20:36 +0200

    FLAPI-1286 Change logic for automated notifcations

--- app/jobs/automated_notification_ios_push_job.rb
+++
@@ -1,7 +0,0 @@
-class AutomatedNotificationIosPushJob < Struct.new(:device_ids, :title, :body, :ttl_hours)
-  include Push
-
-  def perform
-    automated_notification_ios_push(NotificationDeviceId.where(id: device_ids), title, body, ttl_hours)
-  end
-end
--- app/jobs/inactive_seven_days_notification_job.rb
+++ app/jobs/inactive_seven_days_notification_job.rb
@@ -7,12 +7,8 @@ class InactiveSevenDaysNotificationJob
     return unless notification
 
     ActsAsTenant.with_tenant(notification.product) do
-      NotificationDeviceId.joins(:person).where(device_type: :ios).where("people.last_activity_at > ? AND people.last_activity_at < ?", Time.zone.now - 7.day, Time.zone.now - 8.day).select(:id).find_in_batches(batch_size: BATCH_SIZE) do |device_ids|
-        Delayed::Job.enqueue(AutomatedNotificationIosPushJob.new(device_ids.pluck(:id), notification.title, notification.body, notification.ttl_hours))
-      end
-
-      NotificationDeviceId.joins(:person).where(device_type: :android).where("people.last_activity_at > ? AND people.last_activity_at < ?", Time.zone.now - 7.day, Time.zone.now - 8.day).select(:id).find_in_batches(batch_size: BATCH_SIZE) do |device_ids|
-        Delayed::Job.enqueue(AutomatedNotificationAndroidPushJob.new(device_ids.pluck(:id), notification.title, notification.body, notification.ttl_hours))
+      Person.where("last_activity_at > ? AND last_activity_at < ?", Time.zone.now - 7.day, Time.zone.now - 8.day).select(:id).find_in_batches(batch_size: BATCH_SIZE) do |person_ids|
+        Delayed::Job.enqueue(AutomatedNotificationPushJob.new(notification.id, person_ids.pluck(:id)))
       end
     end
   end
