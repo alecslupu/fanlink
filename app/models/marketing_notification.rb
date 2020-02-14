fbf20ee7e14ed9bfbc59a39cdcd6997f5fcc6a04
Cristian Somesan <crs@flink.to>
2020-02-14 18:07:02 +0200

    FLAPI-1286 Solve issue with marketing notifications not being able to be created

--- app/models/marketing_notification.rb
+++ app/models/marketing_notification.rb
@@ -42,8 +42,6 @@ class MarketingNotification < ApplicationRecord
   validates :title, presence: true
   validates :ttl_hours, presence: true, numericality: { greater_than_or_equal_to: 0,  less_than_or_equal_to: 672 }
   validates :person_filter, presence: true
-  validates :product_id, presence: true
-  validates :person_id, presence: true
 
   after_create :notify
 
