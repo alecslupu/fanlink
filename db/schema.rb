e889b57c8bcddbef4bb4487a7168d9fa124169dd
Cristian Somesan <crs@flink.to>
2020-02-21 15:29:35 +0200

    FLAPI-1404 Add date and time zone on marketing notification and admin; add to delete the delayed job created for the notification if the notification has been updated

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200219174349) do
+ActiveRecord::Schema.define(version: 20200221103517) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-  enable_extension "pg_stat_statements"
   enable_extension "pgcrypto"
 
   create_table "action_types", force: :cascade do |t|
@@ -135,10 +134,10 @@ ActiveRecord::Schema.define(version: 20200219174349) do
     t.integer "picture_file_size"
     t.datetime "picture_updated_at"
     t.text "description_text_old"
-    t.jsonb "name", default: {}, null: false
-    t.jsonb "description", default: {}, null: false
     t.datetime "issued_from"
     t.datetime "issued_to"
+    t.jsonb "name", default: {}, null: false
+    t.jsonb "description", default: {}, null: false
     t.index ["action_type_id"], name: "index_badges_on_action_type_id"
     t.index ["issued_from"], name: "ind_badges_issued_from"
     t.index ["issued_to"], name: "ind_badges_issued_to"
@@ -482,6 +481,8 @@ ActiveRecord::Schema.define(version: 20200219174349) do
     t.integer "ttl_hours", default: 672, null: false
     t.integer "person_filter", null: false
     t.string "deep_link", default: "", null: false
+    t.datetime "date", default: -> { "CURRENT_TIMESTAMP" }, null: false
+    t.integer "timezone", null: false
     t.index ["person_id"], name: "index_marketing_notifications_on_person_id"
   end
 
@@ -498,9 +499,9 @@ ActiveRecord::Schema.define(version: 20200219174349) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.boolean "available", default: true, null: false
+    t.integer "priority", default: 0, null: false
     t.jsonb "name", default: {}, null: false
     t.jsonb "description", default: {}, null: false
-    t.integer "priority", default: 0, null: false
     t.boolean "deleted", default: false, null: false
     t.index ["product_id", "priority"], name: "idx_merchandise_product_priority"
     t.index ["product_id"], name: "idx_merchandise_product"
@@ -546,10 +547,8 @@ ActiveRecord::Schema.define(version: 20200219174349) do
     t.datetime "audio_updated_at"
     t.index ["body"], name: "index_messages_on_body"
     t.index ["created_at"], name: "index_messages_on_created_at"
-    t.index ["created_at"], name: "messages_created_at_idx"
     t.index ["person_id"], name: "index_messages_on_person_id"
     t.index ["room_id"], name: "idx_messages_room"
-    t.index ["updated_at"], name: "index_messages_on_updated_at"
   end
 
   create_table "notification_device_ids", force: :cascade do |t|
@@ -613,7 +612,6 @@ ActiveRecord::Schema.define(version: 20200219174349) do
     t.boolean "authorized", default: true, null: false
     t.datetime "last_activity_at"
     t.index ["created_at"], name: "index_people_on_created_at"
-    t.index ["id", "product_id"], name: "index_people_product"
     t.index ["product_id", "auto_follow"], name: "idx_people_product_auto_follow"
     t.index ["product_id", "email"], name: "index_people_on_product_id_and_email"
     t.index ["product_id", "email"], name: "unq_people_product_email", unique: true
@@ -755,7 +753,7 @@ ActiveRecord::Schema.define(version: 20200219174349) do
     t.integer "poll_status", default: 0, null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.datetime "end_date", default: "2019-02-07 01:46:08"
+    t.datetime "end_date", default: "2020-01-31 09:32:07"
     t.jsonb "description", default: {}, null: false
     t.integer "product_id", null: false
     t.index ["poll_type", "poll_type_id"], name: "unq_polls_type_poll_type_id", unique: true
@@ -935,7 +933,6 @@ ActiveRecord::Schema.define(version: 20200219174349) do
     t.string "color_accent", default: "FFF537"
     t.string "color_accent_text", default: "FFF537"
     t.string "color_title_text", default: "FFF537"
-    t.string "color_accessory", default: "000000"
     t.integer "navigation_bar_style", default: 1
     t.integer "status_bar_style", default: 1
     t.integer "toolbar_style", default: 1
@@ -1189,8 +1186,8 @@ ActiveRecord::Schema.define(version: 20200219174349) do
   end
 
   create_table "static_contents", force: :cascade do |t|
-    t.jsonb "content", default: {}, null: false
-    t.jsonb "title", default: {}, null: false
+    t.jsonb "content", default: "{}", null: false
+    t.jsonb "title", default: "{}", null: false
     t.string "slug", null: false
     t.integer "product_id", null: false
     t.datetime "created_at", null: false
