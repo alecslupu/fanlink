763f9d26ef2c472cdaa3cf13fdb40a600572a5e6
Cristian Somesan <crs@flink.to>
2020-02-21 17:34:10 +0200

    Small change

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,6 +1,6 @@
 set :output, { error: "/home/ubuntu/sites/flapi/current/log/error.log", standard: "/home/ubuntu/sites/flapi/current/log/scheduled.log" }
 
-every 2.hour do
+every 5.minute do
   rake "send_activity_notification:two_days_inactive"
 end
 
