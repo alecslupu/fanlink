3dacb40e718d5f5e8fb039c47d560036607b7899
Cristian Somesan <crs@flink.to>
2020-02-21 16:01:32 +0200

    FLAPI-1286 Remove tasks for active notifications

--- config/schedule.rb
+++ config/schedule.rb
@@ -4,23 +4,11 @@ set :output, { error: "/home/ubuntu/sites/flapi/current/log/error.log", standard
 set :environment, "staging"
 
 every 2.hour do
-  rake "send_activity_notification:two_days_active"
-end
-
-every 2.hour do
   rake "send_activity_notification:two_days_inactive"
 end
 
 every :day, at: ["12:00 PM"] do
-  rake "send_activity_notification:seven_days_active"
-end
-
-every :day, at: ["12:00 PM"] do
   rake "send_activity_notification:seven_days_inactive"
-end
-
-every :day, at: ["12:00 PM"] do
-  rake "send_activity_notification:thirty_days_active"
 end
 
 every :day, at: ["12:00 PM"] do
