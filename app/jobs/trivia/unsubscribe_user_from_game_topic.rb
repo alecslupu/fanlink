d5f24bb7ce04db9b7e470313f4d52779e2778270
Cristian Somesan <crs@flink.to>
2020-03-06 12:51:20 +0200

    FLAPI-842 Add to not un/subscribe user to topic by DJ

--- app/jobs/trivia/unsubscribe_user_from_game_topic.rb
+++
@@ -1,7 +0,0 @@
-module Trivia
-  class UnsubscribeUserFromGameTopic < Struct.new(:person_id, :game_id)
-    def perform
-      Push::BasePush.new.unsubscribe_user_from_topic(person_id, game_id)
-    end
-  end
-end
