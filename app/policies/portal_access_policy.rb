24a569880f0ee1540cf5c0c571f7eebbe1c5cb5b
Alexandru-Emil Lupu <contact@alecslupu.ro>
2020-03-19 19:14:37 +0200

    Removing attributes_for from policies

--- app/policies/portal_access_policy.rb
+++ app/policies/portal_access_policy.rb
@@ -1,8 +1,4 @@
 class PortalAccessPolicy < ApplicationPolicy
-  def attributes_for(action)
-    {}
-  end
-
   class Scope < ApplicationPolicy::Scope
     def resolve
       super.for_product(ActsAsTenant.current_tenant)
