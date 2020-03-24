9dfa469792d7c9718444059d6b15846d3c1a357d
Cristian Somesan <crs@flink.to>
2020-03-24 11:25:12 +0000

    Update deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,5 +1,5 @@
 # config valid for current version and patch releases of Capistrano
-lock "~> 3.11.1"
+lock "~> 3.12.1"
 
 set :application, "flapi"
 set :repo_url, "git@gitlab.fan.link:fanlink/fanlink.git"
