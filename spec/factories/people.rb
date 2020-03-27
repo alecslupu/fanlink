3be9387991a9889b1acae0d7bdaacfa9d9a42804
Cristian Somesan <crs@flink.to>
2020-03-27 11:24:40 +0200

    FLAPI-1442 Resolve failing test

--- spec/factories/people.rb
+++ spec/factories/people.rb
@@ -48,7 +48,7 @@ FactoryBot.define do
   factory :person do
     product { current_product }
     sequence(:username) { |n| "person#{n}" }
-    sequence(:email) { |n| "person#{n}@example.com" }
+    sequence(:email) { "#{username}@example.com" }
     name { Faker::Name.name }
     password { "badpassword" }
     birthdate { "2000-01-01" }
