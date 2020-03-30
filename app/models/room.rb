33e5a7f6bca6f19afd7395fa10e140ed073c0531
Alexandru-Emil Lupu <contact@alecslupu.ro>
2020-03-30 17:23:33 +0300

    Migrated clear_message_counter_job to active job

--- app/models/room.rb
+++ app/models/room.rb
@@ -27,7 +27,8 @@ class Room < ApplicationRecord
 
   # old Room::RealTime
   def clear_message_counter(membership)
-    Delayed::Job.enqueue(ClearMessageCounterJob.new(self.id, membership.id))
+    # Delayed::Job.enqueue(ClearMessageCounterJob.new(self.id, membership.id))
+    ClearMessageCounterJob.perform_later(self.id, membership.id)
   end
 
   def delete_me(version = 0)
