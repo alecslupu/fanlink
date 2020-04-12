4c9dce2bff7be28dfbee112c8f9e44c4ef31ac25
Alexandru-Emil Lupu <contact@alecslupu.ro>
2020-04-12 21:23:42 +0300

    Changing the delivery method

--- app/jobs/password_reset_email_job.rb
+++ app/jobs/password_reset_email_job.rb
@@ -1,8 +1,8 @@
 class PasswordResetEmailJob < ApplicationJob
-  queue_as :default
+  queue_as :mailers
 
   def perform(person_id)
     person = Person.find(person_id)
-    PersonMailer.reset_password(person).deliver!
+    PersonMailer.reset_password(person).deliver_now
   end
 end
--- app/jobs/send_certificate_email_job.rb
+++ app/jobs/send_certificate_email_job.rb
@@ -1,10 +1,10 @@
 class SendCertificateEmailJob < ApplicationJob
-  queue_as :default
+  queue_as :mailers
 
   def perform(person_id, certificate_id, email)
     person = Person.find(person_id)
     certificate = PersonCertificate.where(person_id: person_id, certificate_id: certificate_id).last
 
-    PersonMailer.send_certificate(person, certificate, email).deliver!
+    PersonMailer.send_certificate(person, certificate, email).deliver_now
   end
 end
