16e4ee0fc9be54d3e294ca0f543c3ab09ba20bb1
Alexandru-Emil Lupu <contact@alecslupu.ro>
2020-05-15 12:18:02 +0300

    fixing Rails assets

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -3,4 +3,10 @@
 //= link_directory ../stylesheets .css
 //
 // maybe another non-standard file?
-//= link marketing.css
+//= link rails_admin/custom/theming.css
+//= link rails_admin/custom/ui.js
+//= link admin/events.js
+//= link admin/message_reports.js
+//= link admin/post_reports.js
+//= link admin/translated_fields.js
+//= link bootstrap-notify.js
