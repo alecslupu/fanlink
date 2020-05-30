24cd1cdc6f3be9f569f7da967e7bd4ff5b862b38
Alexandru-Emil Lupu <contact@alecslupu.ro>
2020-05-30 08:08:03 +0300

    updating schema

--- db/schema.rb
+++ db/schema.rb
@@ -2,15 +2,15 @@
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
 #
-# Note that this schema.rb definition is the authoritative source for your
-# database schema. If you need to create the application database on another
-# system, you should be using db:schema:load, not running all the migrations
-# from scratch. The latter is a flawed and unsustainable approach (the more migrations
-# you'll amass, the slower it'll run and the greater likelihood for issues).
+# This file is the source Rails uses to define your schema when running `rails
+# db:schema:load`. When creating a new database, `rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_28_041747) do
+ActiveRecord::Schema.define(version: 2020_05_30_050330) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_stat_statements"
@@ -1667,6 +1667,15 @@ ActiveRecord::Schema.define(version: 2020_05_28_041747) do
     t.index ["product_id"], name: "index_urls_on_product_id"
   end
 
+  create_table "version_associations", force: :cascade do |t|
+    t.integer "version_id"
+    t.string "foreign_key_name", null: false
+    t.integer "foreign_key_id"
+    t.string "foreign_type"
+    t.index ["foreign_key_name", "foreign_key_id", "foreign_type"], name: "index_version_associations_on_foreign_key"
+    t.index ["version_id"], name: "index_version_associations_on_version_id"
+  end
+
   create_table "versions", force: :cascade do |t|
     t.text "item_type", null: false
     t.integer "item_id", null: false
@@ -1675,7 +1684,10 @@ ActiveRecord::Schema.define(version: 2020_05_28_041747) do
     t.text "object"
     t.datetime "created_at"
     t.string "locale"
+    t.text "object_changes"
+    t.integer "transaction_id"
     t.index ["item_type", "item_id"], name: "ind_versions_item_type_item_id"
+    t.index ["transaction_id"], name: "index_versions_on_transaction_id"
   end
 
   create_table "video_pages", force: :cascade do |t|
